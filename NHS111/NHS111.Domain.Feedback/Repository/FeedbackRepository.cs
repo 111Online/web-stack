b6ab31cfc7e63e294192c5d679fbbeaa87a437f9
jamestiffen <james.tiffen@hscic.gov.uk>
2016-01-28 15:16:53 +0000

    Added feedback domain project files

---
+++ NHS111/NHS111.Domain.Feedback/Repository/FeedbackRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHS111.Utils.Configuration;
+using NHS111.Utils.Converters;
+
+namespace NHS111.Domain.Feedback.Repository
+{
+    public class FeedbackRepository : IFeedbackRepository
+    {
+
+        private IConnectionManager _sqliteConnectionManager;
+        private IDataConverter<Models.Models.Domain.Feedback> _feedbackConverter;
+
+        public FeedbackRepository(IConnectionManager sqlConnectionManager, IDataConverter<Models.Models.Domain.Feedback> feedbackConverter)
+        {
+            _sqliteConnectionManager = sqlConnectionManager;
+            _feedbackConverter = feedbackConverter;
+        }
+
+        public void Add(Models.Models.Domain.Feedback feedback)
+        {
+            var statemmentParameters = _feedbackConverter.Convert(feedback);
+            var insertQuery = statemmentParameters.GenerateInsertStatement("feedback");
+            _sqliteConnectionManager.ExecteNonQuery(insertQuery, statemmentParameters);
+        }
+
+
+        public async Task<IEnumerable<Models.Models.Domain.Feedback>> List()
+        {
+            var selectStatement = "SELECT feedbackDate,feedbackText,sessionId,pageId,rating,feedbackData " +
+                                  "FROM feedback ORDER BY feedbackDate";
+
+            Task<List<Models.Models.Domain.Feedback>> task = new Task<List<Models.Models.Domain.Feedback>>
+                (
+                () =>
+                {
+                    var feedbackList = new List<Models.Models.Domain.Feedback>();
+                    using ( IManagedDataReader reader = _sqliteConnectionManager.GetReader(selectStatement, new StatementParamaters()))
+                    {
+                        while (reader.Read())
+                        {
+                            feedbackList.Add(_feedbackConverter.Convert(reader));
+                        }
+                    }
+
+                    return feedbackList;
+                });
+            task.Start();
+            return await task;
+
+        }
+    }
+}
