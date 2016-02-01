5861cbe94fab5e4d11591f7aa0ca90632a3e1e93
Tom Axworthy <tom.axworthy@nhs.net>
2016-02-01 11:45:01 +0000

    Add basic view models, models and controller.

---
+++ NHS111/NHS111.Models/Models/Web/FeedbackConfirmation.cs
@@ -0,0 +1,8 @@
+﻿namespace NHS111.Models.Models.Web
+{
+  public class FeedbackConfirmation
+  {
+    public string Message { get; set; }
+    public bool Success { get; set; }
+  }
+}
---
+++ NHS111/NHS111.Web/Controllers/FeedbackController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace NHS111.Web.Controllers
+{
+  public class FeedbackController : Controller
+  {
+    // GET: Feedback
+    [HttpPost]
+    public async Task<ActionResult> SubmitFeedback(Models.Models.Web.Feedback feedbackData)
+    {
+      return View("../Shared/FeedbackConfirmation", await _feedbackBuilder.SubmitDataBuilder(feedbackData));
+    }
+  }
+}
