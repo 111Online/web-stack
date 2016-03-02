b3bec9ed35081f49d68a716321dd980dd3611846
patrick-hughes <patrick.hughes1@hscic.gov.uk>
2016-03-02 17:43:32 +0000

    Added ITK view models

---
+++ NHS111/NHS111.Models/Models/Web/ITK/CaseSummary.cs
@@ -0,0 +1,7 @@
+﻿namespace NHS111.Models.Models.Web.ITK
+{
+    public class CaseSummary
+    {
+        public SummaryItem SummaryItem { get; set; }
+    }
+}
---
+++ NHS111/NHS111.Models/Models/Web/ITK/PatientDetails.cs
@@ -0,0 +1,18 @@
+﻿namespace NHS111.Models.Models.Web.ITK
+{
+    public class PatientDetails
+    {
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public DateOfBirth DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string NhsNumber { get; set; }
+        public string InformationType { get; set; }
+        public string InformationName { get; set; }
+        public Address CurrentAddress { get; set; }
+        public Address HomeAddress { get; set; }
+        public GpPractice GpPractice { get; set; }
+        public string EmailAdress { get; set; }
+        public string TelephoneNumber { get; set; }
+    }
+}
