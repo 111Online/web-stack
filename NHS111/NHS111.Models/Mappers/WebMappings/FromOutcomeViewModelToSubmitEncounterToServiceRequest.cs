719e5adf55d6ea23789fcd8359358096ac6e9076
patrick-hughes <patrick.hughes1@hscic.gov.uk>
2016-03-03 11:12:15 +0000

    Added mapper and tests

---
+++ NHS111/NHS111.Models/Mappers/WebMappings/FromOutcomeViewModelToSubmitEncounterToServiceRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace NHS111.Models.Mappers.WebMappings
+{
+    public class FromOutcomeViewModelToSubmitEncounterToServiceRequest : Profile
+    {
+        protected override void Configure()
+        {
+
+        }
+    }
+}
