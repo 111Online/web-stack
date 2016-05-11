aecfc12fec4386081b3b0ba3ec50c7afa1c14394
jamestiffen <james.tiffen@hscic.gov.uk>
2016-05-11 17:29:16 +0100

    Undone rouge test variable

--- NHS111/NHS111.Domain/Repository/QuestionRepository.cs
+++ NHS111/NHS111.Domain/Repository/QuestionRepository.cs
@@ -60,7 +60,7 @@ namespace NHS111.Domain.Repository
 
         public async Task<QuestionWithAnswers> GetFirstQuestion(string pathwayId)
         {
-            var test =  await _graphRepository.Client.Cypher.
+            return await _graphRepository.Client.Cypher.
                Match(string.Format("(:Pathway {{ id: \"{0}\" }})-[:BeginsWith]->(q)", pathwayId)).
                OptionalMatch("q-[a:Answer]->()").
                Return(q => new QuestionWithAnswers
@@ -70,7 +70,6 @@ namespace NHS111.Domain.Repository
                }).
                ResultsAsync.
                FirstOrDefault();
-            return test;
         }
 
         public async Task<IEnumerable<QuestionWithAnswers>> GetJustToBeSafeQuestions(string pathwayId, string justToBeSafePart)
