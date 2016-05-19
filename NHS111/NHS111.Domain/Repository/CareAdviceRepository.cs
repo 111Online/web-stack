74391eab58049572bd8a1a6c70971184be3f7ffd
jamestiffen <james.tiffen@hscic.gov.uk>
2016-05-19 16:54:36 +0100

    updated to use Cx1910 for worsening care advice

--- NHS111/NHS111.Domain/Repository/CareAdviceRepository.cs
+++ NHS111/NHS111.Domain/Repository/CareAdviceRepository.cs
@@ -24,7 +24,7 @@ namespace NHS111.Domain.Repository
             // TODO How to deal with infant vs. neonate?
 
             return await _graphRepository.Client.Cypher.
-                Match("(c:CareAdviceText)").
+                Match("(c:InterimCareAdvice)").
                 Where(string.Format("c.id in [{0}]", string.Join(",", markers.Select(marker => string.Format("\"{0}-{1}-{2}\"", marker, ageGroup, gender))))).
                 Return(c => c.As<CareAdvice>()).
                 ResultsAsync;
