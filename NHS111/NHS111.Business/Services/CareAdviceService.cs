b066a57128769e3715de189a201617f5381d8387
Adam Naylor <adamnaylor@gmail.com>
2016-05-23 16:26:40 +0100

    Removed swallowing of http status code in business layer care advice.

--- NHS111/NHS111.Business/Services/CareAdviceService.cs
+++ NHS111/NHS111.Business/Services/CareAdviceService.cs
@@ -1,38 +1,43 @@
-﻿using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
-using NHS111.Business.Configuration;
-using NHS111.Utils.Helpers;
-using Newtonsoft.Json;
-using System.Text;
+﻿
+namespace NHS111.Business.Services {
+    using System.Web.Http;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using NHS111.Business.Configuration;
+    using NHS111.Utils.Helpers;
+    using Newtonsoft.Json;
+    using System.Text;
 
-namespace NHS111.Business.Services
-{
-    public class CareAdviceService : ICareAdviceService
-    {
+    public class CareAdviceService
+        : ICareAdviceService {
         private readonly IConfiguration _configuration;
         private readonly IRestfulHelper _restfulHelper;
 
-        public CareAdviceService(IConfiguration configuration, IRestfulHelper restfulHelper)
-        {
+        public CareAdviceService(IConfiguration configuration, IRestfulHelper restfulHelper) {
             _configuration = configuration;
             _restfulHelper = restfulHelper;
         }
 
-        public async Task<string> GetCareAdvice(int age, string gender, IEnumerable<string> markers)
-        {
+        public async Task<string> GetCareAdvice(int age, string gender, IEnumerable<string> markers) {
             return await _restfulHelper.GetAsync(_configuration.GetDomainApiCareAdviceUrl(age, gender, markers));
         }
 
         public async Task<string> GetCareAdvice(string ageCategory, string gender, string keywords, string dxCode) {
-            var request = new HttpRequestMessage { Content = new StringContent(JsonConvert.SerializeObject(keywords), Encoding.UTF8, "application/json") };
-            var response = await _restfulHelper.PostAsync(_configuration.GetDomainApiCareAdviceUrl(dxCode, ageCategory, gender), request);
-            return await response.Content.ReadAsStringAsync(); ;
+            var request = new HttpRequestMessage {
+                Content = new StringContent(JsonConvert.SerializeObject(keywords), Encoding.UTF8, "application/json")
+            };
+            var domainApiCareAdviceUrl = _configuration.GetDomainApiCareAdviceUrl(dxCode, ageCategory, gender);
+            var response = await _restfulHelper.PostAsync(domainApiCareAdviceUrl, request);
+
+            if (!response.IsSuccessStatusCode)
+                throw new HttpResponseException(response);
+
+            return await response.Content.ReadAsStringAsync();
         }
     }
 
-    public interface ICareAdviceService
-    {
+    public interface ICareAdviceService {
         Task<string> GetCareAdvice(int age, string gender, IEnumerable<string> markers);
         Task<string> GetCareAdvice(string ageCategory, string gender, string keywords, string dxCode);
     }
--- NHS111/NHS111.Web.Presentation/Builders/CareAdviceBuilder.cs
+++ NHS111/NHS111.Web.Presentation/Builders/CareAdviceBuilder.cs
@@ -1,63 +1,72 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-using Newtonsoft.Json;
-using NHS111.Models.Models.Domain;
-using NHS111.Utils.Helpers;
-using NHS111.Web.Presentation.Configuration;
+﻿
 
-namespace NHS111.Web.Presentation.Builders
-{
-    public class CareAdviceBuilder : ICareAdviceBuilder
-    {
+namespace NHS111.Web.Presentation.Builders {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using NHS111.Models.Models.Domain;
+    using Utils.Helpers;
+    using Configuration;
+
+    public class CareAdviceBuilder
+        : BaseBuilder, ICareAdviceBuilder {
+
         private readonly IRestfulHelper _restfulHelper;
         private readonly IConfiguration _configuration;
         private const string WORSENING_CAREADVICE_ID = "Cx1910";
 
-        public CareAdviceBuilder(IRestfulHelper restfulHelper, IConfiguration configuration)
-        {
+        public CareAdviceBuilder(IRestfulHelper restfulHelper, IConfiguration configuration) {
             _restfulHelper = restfulHelper;
             _configuration = configuration;
         }
 
-        public async Task<IEnumerable<CareAdvice>> FillCareAdviceBuilder(int age, string gender, IList<string> careAdviceMarkers)
-        {
-            var careAdvices = careAdviceMarkers.Any()
-                 ? JsonConvert.DeserializeObject<List<CareAdvice>>(await _restfulHelper.GetAsync(_configuration.GetBusinessApiCareAdviceUrl(age, gender, string.Join(",", careAdviceMarkers))))
-                 : Enumerable.Empty<CareAdvice>();
+        public async Task<IEnumerable<CareAdvice>> FillCareAdviceBuilder(int age, string gender, IList<string> careAdviceMarkers) {
+            if (!careAdviceMarkers.Any())
+                return Enumerable.Empty<CareAdvice>();
+
+            var businessApiCareAdviceUrl = _configuration.GetBusinessApiCareAdviceUrl(age, gender,
+                string.Join(",", careAdviceMarkers));
+            var response = await _restfulHelper.GetAsync(businessApiCareAdviceUrl);
+            var careAdvices = JsonConvert.DeserializeObject<List<CareAdvice>>(response);
 
             return careAdvices;
         }
 
-        public async Task<CareAdvice> FillWorseningCareAdvice(int age, string gender)
-        {
-            var careAdvices =
-                JsonConvert.DeserializeObject<List<CareAdvice>>(await _restfulHelper.GetAsync(_configuration.GetBusinessApiCareAdviceUrl(age, gender, WORSENING_CAREADVICE_ID)));
-                
+        public async Task<CareAdvice> FillWorseningCareAdvice(int age, string gender) {
+            var businessApiCareAdviceUrl = _configuration.GetBusinessApiCareAdviceUrl(age, gender,
+                WORSENING_CAREADVICE_ID);
+            var response = await _restfulHelper.GetAsync(businessApiCareAdviceUrl);
+            var careAdvices = JsonConvert.DeserializeObject<List<CareAdvice>>(response);
+
             return careAdvices.FirstOrDefault();
         }
 
-        public async Task<IEnumerable<CareAdvice>> FillCareAdviceBuilder(string dxCode, string ageGroup, string gender, IList<string> careAdviceKeywords)
-        {
-            if(!careAdviceKeywords.Any()) return Enumerable.Empty<CareAdvice>();
+        public async Task<IEnumerable<CareAdvice>> FillCareAdviceBuilder(string dxCode, string ageGroup, string gender, IList<string> careAdviceKeywords) {
+            if (!careAdviceKeywords.Any())
+                return Enumerable.Empty<CareAdvice>();
 
-            var request = new HttpRequestMessage { Content = new StringContent(JsonConvert.SerializeObject(GenerateKeywordsList(careAdviceKeywords)), Encoding.UTF8, "application/json") };
-            var response = await (await _restfulHelper.PostAsync(_configuration.GetBusinessApiInterimCareAdviceUrl(dxCode, ageGroup, gender), request)).Content.ReadAsStringAsync();
-            
+            var request = new HttpRequestMessage {
+                Content = new StringContent(JsonConvert.SerializeObject(GenerateKeywordsList(careAdviceKeywords)), Encoding.UTF8, "application/json")
+            };
+
+            var businessApiInterimCareAdviceUrl = _configuration.GetBusinessApiInterimCareAdviceUrl(dxCode, ageGroup, gender);
+            var responseMessage = await _restfulHelper.PostAsync(businessApiInterimCareAdviceUrl, request);
+
+            CheckResponse(responseMessage);
+
+            var response = await responseMessage.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<List<CareAdvice>>(response);
         }
 
-        private string GenerateKeywordsList(IList<string> careAdviceKeywords)
-        {
+        private string GenerateKeywordsList(IList<string> careAdviceKeywords) {
             return careAdviceKeywords.Aggregate((i, j) => i + '|' + j);
         }
     }
 
-    public interface ICareAdviceBuilder
-    {
+    public interface ICareAdviceBuilder {
         Task<IEnumerable<CareAdvice>> FillCareAdviceBuilder(int age, string gender, IList<string> careAdviceMarkers);
 
         Task<IEnumerable<CareAdvice>> FillCareAdviceBuilder(string dxCode, string ageGroup, string gender,
