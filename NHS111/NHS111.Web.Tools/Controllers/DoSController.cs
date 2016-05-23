a26502c7718386ec8d2ad610415c5285a328bf66
patrick-hughes <patrick.hughes1@hscic.gov.uk>
2016-05-23 11:42:46 +0100

    Added web tools project to solution

---
+++ NHS111/NHS111.Web.Tools/Controllers/DoSController.cs
@@ -0,0 +1,31 @@
+﻿using NHS111.Models.Models.Web;
+using NHS111.Utils.Attributes;
+using NHS111.Web.Presentation.Builders;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace NHS111.Web.Tools.Controllers
+{
+    [LogHandleErrorForMVC]
+    public class DoSController : Controller
+    {
+        private readonly IDOSBuilder _dosBuilder;
+
+        public DoSController(IDOSBuilder dosBuilder)
+        {
+            _dosBuilder = dosBuilder;
+        }
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> FillServiceDetails(DosViewModel model)
+        {
+            return View(await _dosBuilder.FillServiceDetailsBuilder(model));
+        }
+    }
+}
