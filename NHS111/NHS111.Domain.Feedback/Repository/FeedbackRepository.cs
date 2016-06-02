69f5aa926d744193d6a24eef60a206d65254e922
Tom Axworthy <tom.axworthy@nhs.net>
2016-06-02 08:06:12 +0100

    :mailbox: Save email address to feedback database

--- NHS111/NHS111.Domain.Feedback/Repository/FeedbackRepository.cs
+++ NHS111/NHS111.Domain.Feedback/Repository/FeedbackRepository.cs
@@ -31,7 +31,7 @@ namespace NHS111.Domain.Feedback.Repository
 
         public async Task<IEnumerable<Models.Models.Domain.Feedback>> List()
         {
-            var selectStatement = "SELECT feedbackDate,feedbackText,sessionId,pageId,rating,feedbackData " +
+            var selectStatement = "SELECT feedbackDate,feedbackText,sessionId,pageId,rating,email,feedbackData " +
                                   "FROM feedback ORDER BY feedbackDate DESC";
 
             Task<List<Models.Models.Domain.Feedback>> task = new Task<List<Models.Models.Domain.Feedback>>
