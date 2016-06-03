c9a2f60971af8b2d661a6e4e3c5c26cc5cf6f12b
Adam Naylor <adamnaylor@gmail.com>
2016-06-03 15:56:04 +0100

    Added new get methods to restfulhelper

--- NHS111/NHS111.Utils/Helpers/RestfulHelper.cs
+++ NHS111/NHS111.Utils/Helpers/RestfulHelper.cs
@@ -1,37 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NHS111.Utils.Helpers
-{
+﻿
+namespace NHS111.Utils.Helpers {
     using System.IO;
-    using System.Web;
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
 
-    public class RestfulHelper : IRestfulHelper
-    {
-        private readonly WebClient _webClient;
-        private readonly HttpClient _httpClient;
+    public class RestfulHelper : IRestfulHelper {
 
-        public RestfulHelper(HttpClient httpClient)
-        {
+        public RestfulHelper(HttpClient httpClient) {
             _httpClient = httpClient;
         }
 
-        public RestfulHelper()
-        {
-            _webClient = new WebClient();
-            _httpClient = new HttpClient();
-
+        public RestfulHelper(HttpClient httpClient, IHttpClientFactory httpClientFactory) {
+            _httpClient = httpClient;
+            _httpClient = httpClient;
+            _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<string> GetAsync(string url)
-        {
-            try
-            {
+        public RestfulHelper() {
+            _webClient = new WebClient();
+            _httpClient = new HttpClient();
+            _httpClientFactory = new HttpClientFactory();
+        }
+
+        public Task<HttpResponseMessage> GetResponseAsync(string url) {
+            return _httpClient.GetAsync(url);
+        }
+
+        public Task<HttpResponseMessage> GetResponseAsync(string url, string username, string password) {
+            var httpClient = _httpClientFactory.Get(username, password);
+            return httpClient.GetAsync(url);
+        }
+
+        public async Task<string> GetAsync(string url) {
+            try {
                 return await _webClient.DownloadStringTaskAsync(new Uri(url));
             }
             catch (WebException e) {
@@ -42,18 +47,14 @@ namespace NHS111.Utils.Helpers
             }
         }
 
-        public async Task<string> GetAsync(string url, string credentials)
-        {
-            try
-            {
+        public async Task<string> GetAsync(string url, string credentials) {
+            try {
                 _webClient.Headers[HttpRequestHeader.Authorization] = credentials;
 
                 return await _webClient.DownloadStringTaskAsync(new Uri(url));
             }
-            catch (WebException e)
-            {
-                using (var stream = new StreamReader(e.Response.GetResponseStream()))
-                {
+            catch (WebException e) {
+                using (var stream = new StreamReader(e.Response.GetResponseStream())) {
                     throw new WebException(
                         string.Format("There was a problem requesting '{0}'; {1}", url, stream.ReadToEnd()), e);
                 }
@@ -61,39 +62,38 @@ namespace NHS111.Utils.Helpers
         }
 
 
-        public async Task<HttpResponseMessage> PostAsync(string url, HttpRequestMessage request)
-        {
+        public async Task<HttpResponseMessage> PostAsync(string url, HttpRequestMessage request) {
             var httpRequestMessage = await BuildRequestMessage(url, request);
             return await _httpClient.SendAsync(httpRequestMessage);
-       
+
         }
 
-        public async Task<HttpResponseMessage> PostAsync(string url, HttpRequestMessage request, Dictionary<string, string> headers)
-        {
+        public async Task<HttpResponseMessage> PostAsync(string url, HttpRequestMessage request,
+            Dictionary<string, string> headers) {
             var httpRequestMessage = await BuildRequestMessage(url, request);
-            foreach (var header in headers)
-            {
+            foreach (var header in headers) {
                 httpRequestMessage.Headers.Add(header.Key, header.Value);
             }
             return await _httpClient.SendAsync(httpRequestMessage);
         }
 
-        private async Task<HttpRequestMessage> BuildRequestMessage(string url, HttpRequestMessage request)
-        {
+        private async Task<HttpRequestMessage> BuildRequestMessage(string url, HttpRequestMessage request) {
             var data = await request.Content.ReadAsStringAsync();
-            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, new Uri(url))
-            {
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, new Uri(url)) {
                 Content = new StringContent(data, Encoding.UTF8, "application/json"),
                 Version = HttpVersion.Version10 //forcing 1.0 to prevent Expect 100 Continue header
             };
             return httpRequestMessage;
         }
 
-    
+        private readonly WebClient _webClient;
+        private readonly HttpClient _httpClient;
+        private readonly IHttpClientFactory _httpClientFactory;
     }
 
-    public interface IRestfulHelper
-    {
+    public interface IRestfulHelper {
+        Task<HttpResponseMessage> GetResponseAsync(string url);
+        Task<HttpResponseMessage> GetResponseAsync(string url, string username, string password);
         Task<string> GetAsync(string url);
         Task<string> GetAsync(string url, string credentials);
         Task<HttpResponseMessage> PostAsync(string url, HttpRequestMessage request);
