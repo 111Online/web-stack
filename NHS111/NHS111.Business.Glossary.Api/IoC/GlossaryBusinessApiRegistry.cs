cc2e9e1193a71290fbb0b53ff43eb960e274713d
jamestiffen <james.tiffen@hscic.gov.uk>
2016-07-04 15:54:00 +0100

    Added Glossary projects

---
+++ NHS111/NHS111.Business.Glossary.Api/IoC/GlossaryBusinessApiRegistry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StructureMap;
+
+namespace NHS111.Business.Glossary.Api.IoC
+{
+    public class GlossaryBusinessApiRegistry : Registry
+    {
+    }
+}
---
+++ NHS111/NHS111.Business.Glossary.Api/IoC/IoC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHS111.Domain.Glossary.IoC;
+using StructureMap;
+
+namespace NHS111.Business.Glossary.Api.IoC
+{
+    public static class IoC
+    {
+        public static IContainer Initialize()
+        {
+            var cont = new Container(c => c.AddRegistry<GlossaryDomainRegistry>());
+            return cont;
+        }
+    }
+}
