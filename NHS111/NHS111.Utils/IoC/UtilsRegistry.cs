e8f6ee4aeb4edfa1e9677df733207a1eaf6ebca4
patrick-hughes <patrick.hughes1@hscic.gov.uk>
2016-07-06 14:57:21 +0100

    Removed feedback api and dependencies

--- NHS111/NHS111.Utils/IoC/UtilsRegistry.cs
+++ NHS111/NHS111.Utils/IoC/UtilsRegistry.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Configuration;
-using System.Data;
-using System.Data.SQLite;
-using KafkaNet;
-using KafkaNet.Model;
-using NHS111.Models.Models.Domain;
-using NHS111.Utils.Cache;
-using NHS111.Utils.Configuration;
-using NHS111.Utils.Converters;
-using NHS111.Utils.Helpers;
+﻿using NHS111.Utils.Helpers;
 using StructureMap;
-using StructureMap.Configuration.DSL;
 using StructureMap.Graph;
 
 namespace NHS111.Utils.IoC
@@ -20,10 +9,7 @@ namespace NHS111.Utils.IoC
         public UtilsRegistry()
         {
             //For<Producer>().Use(new Producer(new BrokerRouter(new KafkaOptions(new Uri("net.tcp://kafka.dev.medplus.steinhauer.technology:9092")))));
-            For<ISqliteConfiguration>().Use<SqliteConfiguration>().Singleton();
-            For<IConnectionManager>().Use<SqliteConnectionManager>().Singleton();
             For<IRestfulHelper>().Use<RestfulHelper>().SelectConstructor(() => new RestfulHelper());
-            For(typeof(IDataConverter<Feedback>)).Use(typeof(FeedbackConverter));
             Scan(scan =>
             {
                 scan.TheCallingAssembly();
