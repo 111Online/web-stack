71bd8f516837de4999c3d6ed5fb8a6fd7457686d
jamestiffen <james.tiffen@hscic.gov.uk>
2016-07-06 10:57:21 +0100

    Updated tests

--- NHS111/NHS111.Domain.Glossary.Tests/DefinitionRepositoryTests.cs
+++ NHS111/NHS111.Domain.Glossary.Tests/DefinitionRepositoryTests.cs
@@ -14,14 +14,18 @@ namespace NHS111.Domain.Glossary.Tests
     public class DefinitionRepositoryTests
     {
 
+        private static string firstDefinitionTerm = "Abdomen";
+        private static string firstDefinitionDescription = "The abdomen relates to the part of the body that extends from underneath the breast area to just above the genitals.";
+        private static string firstDefinitioSynonym = "Tummy";
+
         private string testCsvContent = "GLOSSARYTERM,DESCRIPTION,SYNONYMS" + Environment.NewLine +
-                                        "Abdomen,The abdomen relates to the part of the body that extends from underneath the breast area to just above the genitals.,\\r\\n" + Environment.NewLine +
-                                        "Acne,\"A common skin condition which causes spots and pimples which occur mainly on the face, arms, chest and back. It is caused when the skin produces an oily substance known as sebum. Sebum can block the hair follicles and cause spots.\",\\r\\n" + Environment.NewLine +
-                                        "Agitated,\"A mental state where you feel anxious, irritated and you are not calm.\",Agitation\\r\\n";
+                                        firstDefinitionTerm + "," + firstDefinitionDescription + "," + firstDefinitioSynonym + Environment.NewLine +
+                                        "Acne,\"A common skin condition which causes spots and pimples which occur mainly on the face, arms, chest and back. It is caused when the skin produces an oily substance known as sebum. Sebum can block the hair follicles and cause spots.\"," + Environment.NewLine +
+                                        "Agitated,\"A mental state where you feel anxious, irritated and you are not calm.\",Agitation";
 
         Mock<IFileAdapter> _fileAdapterMock = new Mock<IFileAdapter>();
         [Test()]
-        public void DefinitionRepository_List_Test()
+        public void DefinitionRepository_List_Returns_Terms_Test()
         {
             _fileAdapterMock.Setup(f => f.OpenText()).Returns(GetCsVContentStream());
             var definitionRepository = new DefinitionRepository(new CsvRepostory<DefinitionsMap>(_fileAdapterMock.Object));
@@ -30,11 +34,22 @@ namespace NHS111.Domain.Glossary.Tests
 
             Assert.IsNotNull(result);
             Assert.AreEqual(3,result.Count());
-
-            Assert.AreEqual("Abdomen", result.First().Term);
-
-
         }
+
+        [Test()]
+        public void DefinitionRepository_List_Populates_All_Fields_Test()
+        {
+            _fileAdapterMock.Setup(f => f.OpenText()).Returns(GetCsVContentStream());
+            var definitionRepository = new DefinitionRepository(new CsvRepostory<DefinitionsMap>(_fileAdapterMock.Object));
+
+            var result = definitionRepository.List();
+
+            Assert.AreEqual(firstDefinitionTerm, result.First().Term);
+            Assert.AreEqual(firstDefinitionDescription, result.First().Definition);
+            Assert.AreEqual(firstDefinitioSynonym, result.First().Synonyms);
+        }
+
+
 
      
 
