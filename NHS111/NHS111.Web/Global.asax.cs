82bbff2a8931991f84d01244f24bce9104ba00ad
Adam Naylor <adamnaylor@gmail.com>
2016-07-13 12:12:14 +0100

    Added unhandled exception logging.

--- NHS111/NHS111.Web/Global.asax.cs
+++ NHS111/NHS111.Web/Global.asax.cs
@@ -1,10 +1,15 @@
 ﻿
 namespace NHS111.Web {
+    using System;
+    using System.Collections;
+    using System.Linq;
+    using System.Web;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
     using Models.Models.Web;
     using Presentation.ModelBinders;
+    using Utils.Logging;
 
     public class MvcApplication
         : System.Web.HttpApplication {
@@ -16,6 +21,40 @@ namespace NHS111.Web {
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             ModelBinders.Binders[typeof (JourneyViewModel)] = new JourneyViewModelBinder();
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            try {
+                var currentUrl = "";
+
+                var context = HttpContext.Current;
+                if (context == null)
+                    currentUrl = context.Request.RawUrl;
+
+                var lastException = GetException();
+                if (lastException == null)
+                    return; //nothing to log
+
+                string data = "";
+                foreach (DictionaryEntry pair in lastException.Data)
+                    data += string.Format("{0}: {1}{2}", pair.Key, pair.Value, Environment.NewLine);
+
+                Log4Net.Error(string.Format("{0} occured executing '{1}'{5}{2}{5}{3}{5}Exception.Data:{5}{4}",
+                    lastException.GetType().FullName, currentUrl, lastException.Message,
+                    lastException.StackTrace, data, Environment.NewLine));
+            }
+            catch (Exception ex) {
+                Log4Net.Error(string.Format("Exception occured in OnError: [{0}]", ex.Message));
+            }
+        }
+
+        private Exception GetException() {
+            var lastException = Server.GetLastError();
+            if (lastException == null && HttpContext.Current.AllErrors.Any())
+                lastException = HttpContext.Current.AllErrors.First();
+
+            return lastException;
         }
     }
 }
