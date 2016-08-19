bc4f5d217705109a0014f1fa1cce73c1c26a6960
Adam Naylor <adamnaylor@gmail.com>
2016-08-19 15:47:54 +0100

    Added SD model.

---
+++ NHS111/NHS111.Models/Models/Domain/SymptomDiscriminator.cs
@@ -0,0 +1,8 @@
+﻿
+namespace NHS111.Models.Models.Domain {
+    public class SymptomDiscriminator {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string ReasoningText { get; set; }
+    }
+}
