2d83dba86d251baa2d375cfdd81ad6ffa8fe1327
patrick-hughes <patrick.hughes1@hscic.gov.uk>
2016-09-09 10:50:14 +0100

    Made outcomegroups list static Added outcomegroup id as hidden field

--- NHS111/NHS111.Models/Models/Domain/OutcomeGroup.cs
+++ NHS111/NHS111.Models/Models/Domain/OutcomeGroup.cs
@@ -16,21 +16,21 @@ namespace NHS111.Models.Models.Domain {
 
         public string DefaultTitle { get; set; }
 
-        private readonly Dictionary<string, OutcomeGroup> _outcomeGroups = new Dictionary<string, OutcomeGroup>()
+        public static OutcomeGroup Call999 = new OutcomeGroup { Id = "Call_999", Text = "Call_999", DefaultTitle = "Your answers suggest you need to dial 999 immediately and ask for an ambulance." };
+
+        public static OutcomeGroup AccidentAndEmergency = new OutcomeGroup { Id = "SP_Accident_and_emergency", DefaultTitle = "Your answers suggest you should go to an Accident and Emergency department." };
+
+        public static OutcomeGroup HomeCare = new OutcomeGroup { Id = "Home_Care", Text = "Home Care"};
+
+        public static OutcomeGroup Pharmacy = new OutcomeGroup { Id = "SP_Pharmacy", Text = "Pharmacy", DefaultTitle = "Your answers suggest you should see a pharmacist."};
+
+        private static readonly Dictionary<string, OutcomeGroup> OutcomeGroups = new Dictionary<string, OutcomeGroup>()
         {
             { Call999.Id, Call999 },
             { AccidentAndEmergency.Id, AccidentAndEmergency },
             { HomeCare.Id, HomeCare },
             { Pharmacy.Id, Pharmacy },
         };
-
-        public static OutcomeGroup Call999 = new OutcomeGroup { Id = "Call_999", Text = "Call_999", DefaultTitle = "Your answers suggest you need to dial 999 immediately and ask for an ambulance." };
-
-        public static OutcomeGroup AccidentAndEmergency = new OutcomeGroup { Id = "SP_Accident_and_emergency", DefaultTitle = "Your answers suggest you should go to an Accident and Emergency department within {0}" };
-
-        public static OutcomeGroup HomeCare = new OutcomeGroup { Id = "Home_Care", Text = "Home Care"};
-
-        public static OutcomeGroup Pharmacy = new OutcomeGroup { Id = "SP_Pharmacy", Text = "Pharmacy", DefaultTitle = "Your answers suggest you should see a pharmacist within {0}"};
 
         public override bool Equals(object obj) {
             var outcomeGroup = obj as OutcomeGroup;
@@ -53,7 +53,7 @@ namespace NHS111.Models.Models.Domain {
 
         public string GetDefaultTitle()
         {
-            return _outcomeGroups.ContainsKey(Id) ? _outcomeGroups[Id].DefaultTitle : "Search results";
+            return OutcomeGroups.ContainsKey(Id) ? OutcomeGroups[Id].DefaultTitle : "Search results";
         }
     }
 }
