e92f67616b6560bf5b0928b05a7b46924aa43b05
patrick hughes <patrick.hughes1@hscic.gov.uk>
2016-09-16 12:13:01 +0100

    Added log4net  appender and audit model

---
+++ NHS111/NHS111.Models/Models/Web/Logging/LogEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+
+namespace NHS111.Models.Models.Web.Logging
+{
+    public class LogEntry : TableEntity
+    {
+        public LogEntry()
+        {
+            var now = DateTime.UtcNow;
+            PartitionKey = string.Format("{0:yyyy-MM}", now);
+            RowKey = string.Format("{0:dd HH:mm:ss.fff}-{1}", now, Guid.NewGuid());
+        }
+
+        [JsonProperty(PropertyName = "timestamp")]
+        public DateTime TimeStamp { get; set; }
+
+        [JsonProperty(PropertyName = "sessionId")]
+        public string SessionId { get; set; }
+
+        [JsonProperty(PropertyName = "pathwayId")]
+        public string PathwayId { get; set; }
+
+        [JsonProperty(PropertyName = "pathwayTitle")]
+        public string PathwayTitle { get; set; }
+
+        [JsonProperty(PropertyName = "state")]
+        public string State { get; set; }
+
+        [JsonProperty(PropertyName = "journey")]
+        public string Journey { get; set; }
+    }
+}
---
+++ NHS111/NHS111.Utils/Logging/LogServiceContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using NHS111.Models.Models.Web.Logging;
+
+namespace NHS111.Utils.Logging
+{
+    internal class LogServiceContext
+    {
+        private readonly CloudTable _table;
+        public LogServiceContext(string accountName, string accountKey, string storageTable)
+            : this(new StorageCredentials(accountName, accountKey), storageTable)
+        {
+        }
+
+        public LogServiceContext(StorageCredentials credentials, string storageTable)
+        {
+            var account = new CloudStorageAccount(credentials, true);
+
+            var client = account.CreateCloudTableClient();
+
+            _table = client.GetTableReference(storageTable);
+            _table.CreateIfNotExists();
+        }
+
+        internal void Log(LogEntry logEntry)
+        {
+            var insertOperation = TableOperation.Insert(logEntry);
+            _table.Execute(insertOperation);
+        }
+    }
+}
