6a6ba11ccc690aa7f884e6e406610a9fdcba8872
patrick hughes <patrick.hughes1@hscic.gov.uk>
2016-09-19 15:30:16 +0100

    Added log4net audit state

---
+++ NHS111/NHS111.Utils/Logging/LogAudit.cs
@@ -0,0 +1,22 @@
+﻿using log4net;
+using log4net.Core;
+
+namespace NHS111.Utils.Logging
+{
+    public static class LogAudit
+    {
+        private static readonly Level audit = new Level(50000, "AUDIT");
+
+        public static void Audit(this ILog log, string message)
+        {
+            log.Logger.Log(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType, audit, message, null);
+        }
+
+        public static void Audit(this ILog log, string message, params object[] args)
+        {
+            var formattedMessage = string.Format(message, args);
+            log.Logger.Log(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType, audit, formattedMessage, null);
+        }
+
+    }
+}
