a159dc245fcf165853d5df2c67582b957327eee6
Adam Naylor <adamnaylor@gmail.com>
2016-09-20 14:23:24 +0100

    Removed logdata

--- NHS111/NHS111.Logging.Api/Models/LogData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NHS111.Logging.Api.Models {
-    public class JourneyState {
-        public string SessionId { get; set; }
-        public string JourneyJson { get; set; }
-        public string StateJson { get; set; }
-    }
-}
