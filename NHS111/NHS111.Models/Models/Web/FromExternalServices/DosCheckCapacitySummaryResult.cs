b34ba776ad6ab3af70a28ac8cf6b4f6685be14e0
AdamJamesNaylor <adamnaylor@gmail.com>
2016-10-17 23:10:10 +0100

    Added no dos results message

--- NHS111/NHS111.Models/Models/Web/FromExternalServices/DosCheckCapacitySummaryResult.cs
+++ NHS111/NHS111.Models/Models/Web/FromExternalServices/DosCheckCapacitySummaryResult.cs
@@ -10,5 +10,9 @@ namespace NHS111.Models.Models.Web.FromExternalServices
 
         [JsonProperty(PropertyName = "error")]
         public ErrorObject Error { get; set; }
+
+        public bool HasNoServices {
+            get { return Error != null || (Success != null && Success.Services.Count <= 0); }
+        }
     }
 }
