be721bc9113919ad9e489ed26c4ced5a9fba0f72
patrick hughes <patrick.hughes1@hscic.gov.uk>
2016-11-01 11:31:31 +0000

    Updated cypher query extension to include restriction on starting pathways

--- NHS111/NHS111.Utils/Extensions/CypherFluentQueryExtension.cs
+++ NHS111/NHS111.Utils/Extensions/CypherFluentQueryExtension.cs
@@ -8,20 +8,22 @@ namespace NHS111.Utils.Extensions
     {
         public static ICypherFluentQuery Match(this ICypherFluentQuery query, bool onlyLive = false, params string[] matchText)
         {
-            if (!onlyLive) return query.Match(matchText);
+            if (!onlyLive) return query.Match(matchText).Where("p.startingPathway = true");
 
             return query
                 .Match(matchText)
-                .Where(string.Format("p.title in [{0}]", string.Join(", ", PathwaysConfigurationManager.GetLivePathwaysElements().Select(p => string.Format("'{0}'", p.Title)))));
+                .Where(string.Format("p.title in [{0}]", string.Join(", ", PathwaysConfigurationManager.GetLivePathwaysElements().Select(p => string.Format("'{0}'", p.Title)))))
+                .AndWhere("p.startingPathway = true");
         }
 
         public static ICypherFluentQuery Where(this ICypherFluentQuery query, string matchText, bool onlyLive = false)
         {
-            if (!onlyLive) return query.Where(matchText);
+            if (!onlyLive) return query.Where(matchText).AndWhere("p.startingPathway = true");
 
             return query
                 .Where(matchText)
-                .AndWhere(string.Format("p.title in [{0}]", string.Join(", ", PathwaysConfigurationManager.GetLivePathwaysElements().Select(p => string.Format("'{0}'", p.Title)))));
+                .AndWhere(string.Format("p.title in [{0}]", string.Join(", ", PathwaysConfigurationManager.GetLivePathwaysElements().Select(p => string.Format("'{0}'", p.Title)))))
+                .AndWhere("p.startingPathway = true");
         }
     }
 }
