1dc921e3c3884ce7459e476f32a906bab591d968
patrick hughes <patrick.hughes1@hscic.gov.uk>
2016-11-04 17:45:32 +0000

    Added domain category methods and models

---
+++ NHS111/NHS111.Domain.Api/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using NHS111.Domain.Repository;
+using NHS111.Utils.Attributes;
+using NHS111.Utils.Extensions;
+
+namespace NHS111.Domain.Api.Controllers
+{
+    [LogHandleErrorForApi]
+    public class CategoryController : ApiController
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        [Route("categories")]
+        public async Task<HttpResponseMessage> GetCategories()
+        {
+            return await _categoryRepository.GetCategories().AsJson().AsHttpResponse();
+        }
+
+        [HttpGet]
+        [Route("categories/pathways")]
+        public async Task<HttpResponseMessage> GetAnswersForQuestion()
+        {
+            return await _categoryRepository.GetCategoriesWithPathways().AsJson().AsHttpResponse();
+        }
+
+        [HttpGet]
+        [Route("category/{category}/pathways")]
+        public async Task<HttpResponseMessage> GetNextQuestion(string category)
+        {
+            return await _categoryRepository.GetCategoryWithPathways(category).AsJson().AsHttpResponse();
+        }
+    }
+}
---
+++ NHS111/NHS111.Domain/Repository/CategoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4jClient.Cypher;
+using NHS111.Models.Models.Domain;
+using NHS111.Utils.Extensions;
+
+namespace NHS111.Domain.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly IGraphRepository _graphRepository;
+
+        public CategoryRepository(IGraphRepository graphRepository)
+        {
+            _graphRepository = graphRepository;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _graphRepository.Client.Cypher
+                .Match("(c:Category)")
+                .Return(c => c.As<Category>())
+                .ResultsAsync;
+        }
+
+        public async Task<IEnumerable<CategoryWithPathways>> GetCategoriesWithPathways()
+        {
+            return await _graphRepository.Client.Cypher
+                .Match("(c:Category)-[:hasPathway]->(p:Pathway)")
+                .Return(q => new CategoryWithPathways { Category = Return.As<Category>("c"), Pathways = Return.As<List<Pathway>>("collect(p)") })
+                .ResultsAsync;
+        }
+
+        public async Task<CategoryWithPathways> GetCategoryWithPathways(string category)
+        {
+            return await _graphRepository.Client.Cypher
+                .Match("(c:Category)-[:hasPathway]->(p:Pathway)")
+                .Where(string.Format("c.title = '{0}'", category))
+                .Return(q => new CategoryWithPathways { Category = Return.As<Category>("c"), Pathways = Return.As<List<Pathway>>("collect(p)") })
+                .ResultsAsync
+                .FirstOrDefault();
+        }
+    }
+
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+
+        Task<IEnumerable<CategoryWithPathways>> GetCategoriesWithPathways();
+
+        Task<CategoryWithPathways> GetCategoryWithPathways(string category);
+    }
+}
