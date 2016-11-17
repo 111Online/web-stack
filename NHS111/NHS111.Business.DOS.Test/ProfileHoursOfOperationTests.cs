78d213e3bdc32c56793d869bf6729c8d11454a6a
jamestiffen <james.tiffen@hscic.gov.uk>
2016-11-17 15:27:11 +0000

    added Profile OOH tests

---
+++ NHS111/NHS111.Business.DOS.Test/ProfileHoursOfOperationTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NHS111.Business.DOS;
+using NHS111.Business.DOS.Configuration;
+using NHS111.Models.Models.Business;
+using NodaTime;
+using NUnit.Framework;
+namespace NHS111.Business.DOS.Tests
+{
+    [TestFixture()]
+    public class ProfileHoursOfOperationTests
+    {
+        private Mock<IConfiguration> _mockConfiguration = new Mock<IConfiguration>();
+        private ProfileHoursOfOperation _profileHoursOfOperation;
+
+        [SetUp]
+        private void SetupCondfig()
+        {
+
+            _mockConfiguration.SetupGet(c => c.WorkingDayInHoursEndTime).Returns(new LocalTime(18,0));
+            _mockConfiguration.SetupGet(c => c.WorkingDayInHoursShoulderEndTime).Returns(new LocalTime(9, 0));
+            _mockConfiguration.SetupGet(c => c.WorkingDayInHoursStartTime).Returns(new LocalTime(8, 0));
+            _profileHoursOfOperation = new ProfileHoursOfOperation(_mockConfiguration.Object);
+        }
+
+        [Test()]
+        public void GeServiceTime_Weekend_Test()
+        {
+            var result = _profileHoursOfOperation.GeServiceTime(new DateTime(2016, 11, 19));
+            Assert.AreEqual(ProfileServiceTimes.OutOfHours, result);
+        }
+    }
+}
---
+++ NHS111/NHS111.Business.DOS/Configuration/Configuration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHS111.Models.Models.Web.FromExternalServices;
+using NodaTime;
+using NodaTime.Text;
+
+namespace NHS111.Business.DOS.Configuration
+{
+    public class Configuration : IConfiguration
+    {
+
+        public LocalTime WorkingDayInHoursStartTime
+        {
+            get { return Get(ConfigurationManager.AppSettings["WorkingDayInHoursStartTime"]); }
+        }
+
+        public LocalTime WorkingDayInHoursEndTime
+        {
+            get { return Get(ConfigurationManager.AppSettings["WorkingDayInHoursEndTime"]); }
+        }
+
+        public LocalTime WorkingDayInHoursShoulderEndTime
+        {
+            get { return Get(ConfigurationManager.AppSettings["WorkingDayInHoursShoulderEndTime"]); }
+        }
+
+        private LocalTime Get(string configText)
+        {
+           var parser =  LocalTimePattern.CreateWithCurrentCulture("HH:mm").Parse(configText);
+           return parser.GetValueOrThrow();
+        }
+    }
+}
