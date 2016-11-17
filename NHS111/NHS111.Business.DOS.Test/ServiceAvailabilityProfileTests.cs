a5dd53465399c8f99f20323dafe986aeabb4147a
jamestiffen <james.tiffen@hscic.gov.uk>
2016-11-17 12:00:54 +0000

    added OOH to OOH test

--- NHS111/NHS111.Business.DOS.Test/ServiceAvailabilityProfileTests.cs
+++ NHS111/NHS111.Business.DOS.Test/ServiceAvailabilityProfileTests.cs
@@ -30,7 +30,6 @@ namespace NHS111.Business.DOS.Test
         }
 
         [Test()]
-
         public void GetServiceAvailability_In_Hours_And_Timeframe_Out_of_hours_Test()
         {
             var result = _serviceAvailabilityProfile.GetServiceAvailability(InHoursToOoHoursPeriodWeekday.Item1,
@@ -39,12 +38,19 @@ namespace NHS111.Business.DOS.Test
         }
 
         [Test()]
-
         public void GetServiceAvailability_Out_of_Hours_And_Timeframe_In_hours_Test()
         {
             var result = _serviceAvailabilityProfile.GetServiceAvailability(OoHoursToInHoursPeriodWeekday.Item1,
                 OoHoursToInHoursPeriodWeekday.Item2);
             Assert.AreEqual(DispositionTimePeriod.DispositionOutOfHoursTimeFrameInHours, result);
+        }
+
+        [Test()]
+        public void GetServiceAvailability_Out_of_Hours_And_Timeframe_Out_of_hours_Test()
+        {
+            var result = _serviceAvailabilityProfile.GetServiceAvailability(OOHoursToOOHoursPeriodWeekday.Item1,
+                OOHoursToOOHoursPeriodWeekday.Item2);
+            Assert.AreEqual(DispositionTimePeriod.DispositionAndTimeFrameOutOfHours, result);
         }
     }
 
