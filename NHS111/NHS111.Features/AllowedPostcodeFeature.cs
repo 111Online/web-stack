e602cde27ef2dad319760eb05de24daefa1e753a
patrick hughes <patrick.hughes1@hscic.gov.uk>
2016-12-07 10:32:13 +0000

    Added allowed postcodes feature

---
+++ NHS111/NHS111.Features/AllowedPostcodeFeature.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHS111.Features.Defaults;
+
+namespace NHS111.Features
+{
+    public class AllowedPostcodeFeature : BaseFeature, IAllowedPostcodeFeature
+    {
+
+        public AllowedPostcodeFeature()
+        {
+            DefaultIsEnabledSettingStrategy = new EnabledByDefaultSettingStrategy();
+        }
+
+        public string PostcodeFilePath
+        {
+            get { return FeatureValue(new PostcodeFilePathDefaultSettingStrategy(), "PostcodeFilePath").Value; }
+        }
+    }
+
+    public interface IAllowedPostcodeFeature : IFeature
+    {
+        string PostcodeFilePath { get; }
+    }
+}
