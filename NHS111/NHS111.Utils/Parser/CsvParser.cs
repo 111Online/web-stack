cd371956cb62321f5268ed3ccc0a2d42129e054d
patrick hughes <patrick.hughes1@hscic.gov.uk>
2016-12-07 10:20:50 +0000

    Added csv reader and files

---
+++ NHS111/NHS111.Utils/Parser/CsvParser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHS111.Utils.Parser
+{
+    public class CsvParser : ICsvParser
+    {
+        public CsvParser(string filePath) : this(filePath, false)
+        {
+        
+        }
+
+        public CsvParser(string filePath, bool hasHeader)
+        {
+            if (!File.Exists(filePath)) throw new FileNotFoundException();
+
+            Lines = hasHeader ? File.ReadLines(filePath).Skip(1) : File.ReadLines(filePath);
+        }
+
+        public IEnumerable<string> Lines { get; set; }
+
+        public IEnumerable<string> GetColumnValues(int columnIndex)
+        {
+            if(columnIndex < 0) throw  new IndexOutOfRangeException();
+
+            var csv = Lines.Select(line => line.Split(',')).ToList();
+
+            if(csv.Any() && Lines.First().Length < columnIndex) throw new IndexOutOfRangeException();
+
+            return csv.Select(c => c[columnIndex]);
+        }
+    }
+
+    public interface ICsvParser
+    {
+        IEnumerable<string> Lines { get; }
+
+        IEnumerable<string> GetColumnValues(int columnIndex);
+    } 
+}
