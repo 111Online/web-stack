8fde3306365508fa24a16ae202d12fad7bdc6e38
Patrick <patrick.hughes@digital.nhs.uk>
2017-01-06 15:06:42 +0000

    added initial survey link banner

--- build-development.bat
+++ build-development.bat
@@ -2,9 +2,9 @@
 
 setlocal enabledelayedexpansion
 
-set MSBUILDER="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+set MSBUILDER="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 set NUGET="NHS111\.NuGet\NuGet.exe"
-set OUTPUTDIR="L:\Work\111 Online\nhs111-build-dir\nhs111-beta"
+set OUTPUTDIR="C:\Work\nhs111-build-dir\nhs111-beta"
 
 %NUGET% restore NHS111\NHS111.sln
 
