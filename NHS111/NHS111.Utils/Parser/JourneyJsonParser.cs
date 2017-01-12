4425a6565a9f2bbc82eb4c3cad0f0a5e417e7572
patrick hughes <patrick.hughes1@hscic.gov.uk>
2017-01-12 17:00:30 +0000

    added model and builder for survey view

--- NHS111/NHS111.Utils/Parser/JourneyJsonParser.cs
+++ NHS111/NHS111.Utils/Parser/JourneyJsonParser.cs
@@ -8,14 +8,32 @@ using NHS111.Models.Models.Web.FromExternalServices;
 
 namespace NHS111.Utils.Parser
 {
-    public static class JourneyJsonParser
+    public class JourneyJsonParser
     {
-        public static string GetEndingPathway(string journeyJson)
+        public JourneyJsonParser(string journeyJson)
         {
             var journey = JsonConvert.DeserializeObject<Journey>(journeyJson);
-            var endingPathway = journey.Steps.Last();
+            JourneySteps = journey.Steps;
+        }
 
-            return endingPathway.QuestionId.Split('.').FirstOrDefault();
+        public IEnumerable<JourneyStep> JourneySteps { get; private set; }
+
+        public string LastPathwayNo
+        {
+            get
+            {
+                var endingPathway = JourneySteps.Last();
+                return endingPathway.QuestionId.Split('.').FirstOrDefault();
+            }
+        }
+
+        public string LastPathwayTitle
+        {
+            get
+            {
+                var endingPathway = JourneySteps.Last();
+                return endingPathway.QuestionId.Split('.').FirstOrDefault();
+            }
         }
     }
 }
