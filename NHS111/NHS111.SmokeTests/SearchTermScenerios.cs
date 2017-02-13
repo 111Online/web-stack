fa1542ab758f007f0e1d7594310204409af220c8
patrick hughes <patrick.hughes1@hscic.gov.uk>
2017-02-13 15:07:51 +0000

    updated expected titles after index update

--- NHS111/NHS111.SmokeTests/SearchTermScenerios.cs
+++ NHS111/NHS111.SmokeTests/SearchTermScenerios.cs
@@ -23,12 +23,12 @@ namespace NHS111.SmokeTests
             _testTerms = new List<Tuple<string, string>>()
             {
                 new Tuple<string, string>("tummy pain","Abdominal Pain"), //abdo pain
-                new Tuple<string, string>("pain in my tooth","Toothache and other dental problems"), //dental
+                new Tuple<string, string>("pain in my tooth","Toothache, swelling and other dental problems"), //dental
                 new Tuple<string, string>("diorea","Diarrhoea and vomiting"), // mispelling
                 new Tuple<string, string>("stomach ache","Abdominal Pain"), //synonym
-                new Tuple<string, string>("Head ache","Headache"), //synonym
-                new Tuple<string, string>("diarhoea","Diarrhoea - no vomiting"), //misspelling not on list
-                new Tuple<string, string>("vomitting","Vomiting and nausea - no diarrhoea"), //misspelling
+                new Tuple<string, string>("Head ache","Headache and migraine"), //synonym
+                new Tuple<string, string>("diarhoea","Diarrhoea"), //misspelling not on list
+                new Tuple<string, string>("vomitting","Vomiting or nausea"), //misspelling
                 new Tuple<string, string>("under","Something under the skin"), //Appears in digital title only not description
                 new Tuple<string, string>("swallowing","Difficulty swallowing"), //Appears in description only not title
                 new Tuple<string, string>("lumps","Rashes, itching, spots, moles and other skin problems"), //Appears in digital description only
