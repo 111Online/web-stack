3d4f6fba5d96b295abe3b7da25f89ad384302a4a
AdamJamesNaylor <adamnaylor@gmail.com>
2017-02-16 14:47:19 +0000

    Added highlight into view model

--- NHS111/NHS111.Models/Models/Web/SearchResultViewModel.cs
+++ NHS111/NHS111.Models/Models/Web/SearchResultViewModel.cs
@@ -4,6 +4,7 @@
     public class SearchResultViewModel {
         public string PathwayTitle { get; set; }
         public List<string> Title { get; set; }
+        public List<string> HighlightedTitle { get; set; }
         public object TitlePhonetic { get; set; }
         public string Description { get; set; }
         public object DescriptionPhonetic { get; set; }
