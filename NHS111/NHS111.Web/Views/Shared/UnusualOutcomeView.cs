6be531989d74982c3bf22f414857a71d38929ba2
Tom Axworthy <tom.axworthy@nhs.net>
2017-03-06 16:37:41 +0000

    Update outcome views to use partial view in same way

---
+++ NHS111/NHS111.Web/Views/Shared/UnusualOutcomeView.cs
@@ -0,0 +1,21 @@
+﻿using NHS111.Features;
+
+namespace NHS111.Web.Views.Shared
+{
+    using System.Web.Mvc;
+
+    public class UnusualOutcomeView<T>
+        : WebViewPage<T>
+    {
+        protected readonly ISurveyLinkFeature SurveyLinkFeature;
+        protected readonly IUserZoomSurveyFeature UserZoomSurveyFeature;
+
+        public UnusualOutcomeView()
+        {
+            SurveyLinkFeature = new SurveyLinkFeature();
+            UserZoomSurveyFeature = new UserZoomSurveyFeature();
+        }
+
+        public override void Execute() { }
+    }
+}
