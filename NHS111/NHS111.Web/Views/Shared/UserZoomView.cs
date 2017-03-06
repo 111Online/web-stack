bf570616883e7ae17060155de1a625df4c1d663d
Tom Axworthy <tom.axworthy@nhs.net>
2017-03-06 11:20:35 +0000

    Run javascript on every outcome page when disposition is shown

---
+++ NHS111/NHS111.Web/Views/Shared/UserZoomView.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using NHS111.Features;
+
+namespace NHS111.Web.Views.Shared
+{
+    public class UserZoomView<T> : WebViewPage<T>
+    {
+        public IUserZoomSurveyFeature UserZoomSurveyFeature { get; set; }
+
+        public UserZoomView()
+        {
+            UserZoomSurveyFeature = new UserZoomSurveyFeature();
+        }
+
+        public override void Execute() { }
+    }
+}
