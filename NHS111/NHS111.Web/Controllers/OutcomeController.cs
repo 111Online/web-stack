3e658399cafb4e7ed834a85bd328b765d1c1595e
Patrick <patrick.hughes@digital.nhs.uk>
2017-03-19 15:34:47 +0000

    added logging for bounce rate

--- NHS111/NHS111.Web/Controllers/OutcomeController.cs
+++ NHS111/NHS111.Web/Controllers/OutcomeController.cs
@@ -193,7 +193,7 @@ namespace NHS111.Web.Controllers {
         private void AuditDosResponse(OutcomeViewModel model) {
             var audit = model.ToAuditEntry();
             var auditedDosResponse = Mapper.Map<AuditedDosResponse>(model.DosCheckCapacitySummaryResult);
-            audit.DosResponse = JsonConvert.SerializeObject(model.DosCheckCapacitySummaryResult);
+            audit.DosResponse = JsonConvert.SerializeObject(auditedDosResponse);
             _auditLogger.Log(audit);
         }
 
