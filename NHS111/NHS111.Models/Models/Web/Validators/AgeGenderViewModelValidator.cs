49d37ba046cf872cb219379dbdbf6b20631ae62a
patrick hughes <patrick.hughes1@hscic.gov.uk>
2017-04-27 14:22:23 +0100

    :mens: :womens: add validation for gender

--- NHS111/NHS111.Models/Models/Web/Validators/AgeGenderViewModelValidator.cs
+++ NHS111/NHS111.Models/Models/Web/Validators/AgeGenderViewModelValidator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
+using NHS111.Models.Models.Domain;
 
 namespace NHS111.Models.Models.Web.Validators
 {
@@ -11,6 +12,8 @@ namespace NHS111.Models.Models.Web.Validators
     {
         public AgeGenderViewModelValidator()
         {
+            RuleFor(p => p.Gender).Cascade(CascadeMode.Continue)
+                .NotNull();
             RuleFor(p => p.Age)
                 .SetValidator(new AgeValidator<AgeGenderViewModel, int>(u => u.Age));
         }
