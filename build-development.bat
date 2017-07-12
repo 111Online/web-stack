72ea2201512b5b8cd4c3b24401c9824a0a7c84fc
Patrick <patrick.hughes@digital.nhs.uk>
2017-07-12 14:24:51 +0100

    updated build script to use msbuild 12

--- build-development.bat
+++ build-development.bat
@@ -2,7 +2,7 @@
 
 setlocal enabledelayedexpansion
 
-set MSBUILDER="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+set MSBUILDER="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
 set NUGET="NHS111\.NuGet\NuGet.exe"
 set OUTPUTDIR="C:\Work\nhs111-build-dir\nhs111-beta"
 
