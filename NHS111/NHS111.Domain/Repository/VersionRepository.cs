bb8aeea732b19ec9eb9fa420722b9193de634394
patrick hughes <patrick.hughes1@hscic.gov.uk>
2017-07-20 16:26:17 +0100

    added version info controller and view

---
+++ NHS111/NHS111.Domain/Repository/VersionRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHS111.Models.Models.Domain;
+using NHS111.Utils.Extensions;
+
+namespace NHS111.Domain.Repository
+{
+    public class VersionRepository
+    {
+        public VersionRepository(IGraphRepository graphRepository)
+        {
+            _graphRepository = graphRepository;
+        }
+
+        public async Task<VersionInfo> GetInfo()
+        {
+            return await _graphRepository.Client.Cypher
+                .Return(v => v.As<VersionInfo>())
+                .ResultsAsync
+                .FirstOrDefault();
+        }
+
+        private readonly IGraphRepository _graphRepository;
+    }
+
+    public interface IVersionRepository
+    {
+        Task<VersionInfo> GetInfo();
+    }
+}
--- NHS111/NHS111.Web/Controllers/HelpController.cs
+++ NHS111/NHS111.Web/Controllers/HelpController.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using NHS111.Models.Models.Domain;
+using NHS111.Web.Presentation.Configuration;
+using RestSharp;
 
 namespace NHS111.Web.Controllers
 {
     public class HelpController : Controller
     {
+        public HelpController(IConfiguration configuration, IRestClient restClientBusinessApi)
+        {
+            _configuration = configuration;
+            _restClientBusinessApi = restClientBusinessApi;
+        }
+
         public ActionResult Cookies()
         {
             return View();
@@ -27,5 +37,21 @@ namespace NHS111.Web.Controllers
         {
             return View();
         }
+
+        public async Task<PartialViewResult> VersionInfo()
+        {
+            var url = _configuration.GetBusinessApiVersionInfoUrl();
+            var request = new RestRequest(url, Method.GET);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            
+            //var response = await _restClientBusinessApi.ExecuteTaskAsync<VersionInfo>(request);
+
+            var response = new RestResponse<VersionInfo>() { Data = new VersionInfo() { Build = "v111", Date = DateTime.Now, ContentBranch = "/content/branch", DataBranch = "/data/branch", Version = "v11" } };
+
+            return PartialView("_VersionInfo", response.Data);
+        }
+
+        private readonly IConfiguration _configuration;
+        private readonly IRestClient _restClientBusinessApi;
     }
 }
