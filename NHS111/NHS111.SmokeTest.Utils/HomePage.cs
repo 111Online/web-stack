ae3bcbfa20e3c8083df7f8bd4187cd2a44bb1839
jamestiffen <james.tiffen@hscic.gov.uk>
2017-08-02 12:45:31 +0100

    check url contains credentials before redirecting

--- NHS111/NHS111.SmokeTest.Utils/HomePage.cs
+++ NHS111/NHS111.SmokeTest.Utils/HomePage.cs
@@ -34,13 +34,26 @@ namespace NHS111.SmokeTest.Utils
         public void Load()
         {
             _driver.Navigate().GoToUrl(_baseUrl);
-            _driver.Navigate().GoToUrl(GetUrlWithoutCredentials());
+            if (UrlContainsCredentials())
+            {
+                _driver.Navigate().GoToUrl(GetUrlWithoutCredentials());
+            }
             _driver.Manage().Window.Maximize();
         }
 
         private string GetUrlWithoutCredentials()
         {
-            return _driver.Url.Remove(_baseUrl.IndexOf("://") + 3, _baseUrl.LastIndexOf("@") - (_baseUrl.IndexOf("://") + 3));
+            if (UrlContainsCredentials())
+            {
+                return _driver.Url.Remove(_baseUrl.IndexOf("://") + 3,
+                    _baseUrl.LastIndexOf("@") - (_baseUrl.IndexOf("://") + 3));
+            }
+            return _baseUrl;
+        }
+
+        private static bool UrlContainsCredentials()
+        {
+            return _baseUrl.Contains("@");
         }
 
         public ModuleZeroPage ClickNextButton()
