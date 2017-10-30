64cf2ab5aa93c1bf7fe5ed356db332db37bae7ad
Shane Hudson <shane@shanehudson.net>
2017-10-30 12:26:23 +0000

    This fixes issues (mostly due to a broken babel config) that caused IE9-11 to break for JavaScript.

--- NHS111/NHS111.Web.Frontend/src/js/entry-main.js
+++ NHS111/NHS111.Web.Frontend/src/js/entry-main.js
@@ -4,5 +4,15 @@ import './vendor/jquery.validate.unobtrusive.js'
 import './vendor/jquery.unobtrusive-ajax.js'
 import './vendor/jquery.ns-autogrow.js'
 
-global.validation = require('./validation')
+//global.validation = require('./validation')
 global.geolocation = require('./geolocation')
+
+
+jQuery.validator.setDefaults({
+    highlight: function (element, errorClass, validClass) {
+        $(element).closest(".form-group").addClass("form-group-error");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+        $(element).closest(".form-group").removeClass("form-group-error");
+    }
+});
