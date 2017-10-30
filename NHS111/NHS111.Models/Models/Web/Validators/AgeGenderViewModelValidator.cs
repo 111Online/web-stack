dcf83bee19bf10fd05ed6267174dee97edec6cbb
Adam Naylor <anaylor@Development.HealthyChoices.gsi.gov.uk>
2017-10-30 11:06:03 +0000

    Adds NotEmpty().WithMessage() to allow override of default messaging.

--- NHS111/NHS111.Models/Models/Web/Validators/AgeGenderViewModelValidator.cs
+++ NHS111/NHS111.Models/Models/Web/Validators/AgeGenderViewModelValidator.cs
@@ -9,11 +9,13 @@ namespace NHS111.Models.Models.Web.Validators
 {
     public class AgeGenderViewModelValidator : AbstractValidator<AgeGenderViewModel>
     {
-        public AgeGenderViewModelValidator()
-        {
+        public AgeGenderViewModelValidator() {
             RuleFor(p => p.Gender)
-                .NotEmpty();
+                .NotEmpty()
+                .WithMessage("'Sex' must not be empty.");
             RuleFor(p => p.Age)
+                .NotEmpty()
+                .WithMessage("'Age' must not be empty.")
                 .SetValidator(new AgeValidator<AgeGenderViewModel, int>(u => u.Age))
                 .WithMessage("Sorry, this service is not available for children under 5 years of age, for medical advice please call 111.")
                 .LessThan(201).WithMessage("The age you entered is incorrect")
