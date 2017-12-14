6ab81a68066aa16c2928f488c8110c358afb072b
Patrick Hughes <patrick.hughes1@nhs.net>
2017-12-14 15:31:01 +0000

    fixes session id retention across multiple hosts

--- NHS111/NHS111.Utils/Filters/SetSessionIdFilterAttribute.cs
+++ NHS111/NHS111.Utils/Filters/SetSessionIdFilterAttribute.cs
@@ -1,4 +1,6 @@
-﻿namespace NHS111.Utils.Filters {
+﻿using System.Web;
+
+namespace NHS111.Utils.Filters {
     using System;
     using System.Web.Mvc;
     using Models.Models.Web;
@@ -14,7 +16,17 @@
             if (model == null) 
                 return;
 
-            model.SessionId = Guid.Parse(filterContext.RequestContext.HttpContext.Request.AnonymousID);
+            var sessionIdCookie = filterContext.RequestContext.HttpContext.Request.Cookies["nhs111-session-id"];
+            if (sessionIdCookie == null)
+            {
+                sessionIdCookie = new HttpCookie("nhs111-session-id")
+                {
+                    Value = filterContext.RequestContext.HttpContext.Request.AnonymousID,
+                    Expires = DateTime.Now.AddHours(72)
+                };
+                filterContext.RequestContext.HttpContext.Response.Cookies.Add(sessionIdCookie);
+            }
+            model.SessionId = Guid.Parse(sessionIdCookie.Value);
         }
     }
 }
