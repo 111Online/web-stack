e8e83840e791cd64ef15c3aa8ca88d1d14e92c3b
Adam Naylor <adamnaylor@gmail.com>
2018-01-04 16:09:41 +0000

    Amended thank you message

--- NHS111/NHS111.Models/Models/Web/FeedbackConfirmation.cs
+++ NHS111/NHS111.Models/Models/Web/FeedbackConfirmation.cs
@@ -4,9 +4,8 @@ namespace NHS111.Models.Models.Web
 {
   public class FeedbackConfirmation
   {
-        private static string _successMessage = @"Thank you.<br>
-                                                  We use feedback to improve the service, but can't reply to any comments.<br>
-                                                  There’s a survey after the symptom questions, where you can give more detailed feedback if you like.";
+        private static string _successMessage = @"<p>Thank you.</p>
+                                                  <p>If you want to give more detailed feedback, there’s a survey after the symptom questions.</p>";
         private static string _errorMessage = "Sorry, there is a technical problem. Try again in a few moments.";
 
         [JsonProperty(PropertyName = "message")]
