60d01a1b436aa5c15200d1bb716251b8a090caaf
Adam Naylor <adamnaylor@gmail.com>
2018-01-04 16:16:09 +0000

    Changes to br

--- NHS111/NHS111.Models/Models/Web/FeedbackConfirmation.cs
+++ NHS111/NHS111.Models/Models/Web/FeedbackConfirmation.cs
@@ -4,8 +4,7 @@ namespace NHS111.Models.Models.Web
 {
   public class FeedbackConfirmation
   {
-        private static string _successMessage = @"<p>Thank you.</p>
-                                                  <p>If you want to give more detailed feedback, there’s a survey after the symptom questions.</p>";
+        private static string _successMessage = @"<p>Thank you.<br />If you want to give more detailed feedback, there’s a survey after the symptom questions.</p>";
         private static string _errorMessage = "Sorry, there is a technical problem. Try again in a few moments.";
 
         [JsonProperty(PropertyName = "message")]
