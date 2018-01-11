7da94dc2ce2f836bcb3b5a93915314eef4fb7394
jamestiffen <james.tiffen@hscic.gov.uk>
2018-01-11 13:30:21 +0000

    implimented new openingtimes partial in callback and goto services views

--- NHS111/NHS111.Models.Test/Models/Web/ServiceViewModelTests/OpeningTimesTests.cs
+++ NHS111/NHS111.Models.Test/Models/Web/ServiceViewModelTests/OpeningTimesTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace NHS111.Models.Test.Models.Web.ServiceViewModelTests
@@ -182,6 +183,62 @@ namespace NHS111.Models.Test.Models.Web.ServiceViewModelTests
 
             Assert.AreEqual("Open today: 13:00 until 18:00", service.CurrentStatus);
             Assert.AreEqual("Open today: 13:00 until 18:00", service.ServiceOpeningTimesMessage);
+        }
+
+        [Test]
+        public void NextOpenDayRotaSessions_returns_all_session_for_current_day()
+        {
+            var clock = new StaticClock(DayOfWeek.Thursday, 09, 05);
+            var service = new NHS111.Models.Models.Web.ServiceViewModel(clock)
+            {
+                OpenAllHours = false,
+                RotaSessions = new[]
+                {
+                    _serviceViewModelTestHelper.MONDAY_SESSION,
+                    _serviceViewModelTestHelper.TUESDAY_SESSION,
+                    _serviceViewModelTestHelper.WEDNESDAY_SESSION,
+                    _serviceViewModelTestHelper.THURSDAY_MORNING_SESSION,
+                    _serviceViewModelTestHelper.THURSDAY_AFTERNOON_SESSION,
+                    _serviceViewModelTestHelper.FRIDAY_SESSION
+                }
+            };
+
+            var todaysOpeningTimes = service.NextOpenDayRotaSessions;
+            Assert.AreEqual(2,todaysOpeningTimes.Count());
+            Assert.AreEqual(DayOfWeek.Thursday, todaysOpeningTimes.First().Day);
+            Assert.IsTrue(_serviceViewModelTestHelper.IsSameTime(todaysOpeningTimes.First().OpeningTime, _serviceViewModelTestHelper.THURSDAY_MORNING_SESSION.StartTime));
+            Assert.IsTrue(_serviceViewModelTestHelper.IsSameTime(todaysOpeningTimes.First().ClosingTime, _serviceViewModelTestHelper.THURSDAY_MORNING_SESSION.EndTime));
+
+
+            Assert.AreEqual(DayOfWeek.Thursday, todaysOpeningTimes.ToList()[1].Day);
+            Assert.IsTrue(_serviceViewModelTestHelper.IsSameTime(todaysOpeningTimes.ToList()[1].OpeningTime, _serviceViewModelTestHelper.THURSDAY_AFTERNOON_SESSION.StartTime));
+            Assert.IsTrue(_serviceViewModelTestHelper.IsSameTime(todaysOpeningTimes.ToList()[1].ClosingTime, _serviceViewModelTestHelper.THURSDAY_AFTERNOON_SESSION.EndTime));
+
+        }
+
+        [Test]
+        public void NextOpenDayRotaSessions_returns_all_session_for_next_day()
+        {
+            var clock = new StaticClock(DayOfWeek.Thursday, 13, 05);
+            var service = new NHS111.Models.Models.Web.ServiceViewModel(clock)
+            {
+                OpenAllHours = false,
+                RotaSessions = new[]
+                {
+                    _serviceViewModelTestHelper.MONDAY_SESSION,
+                    _serviceViewModelTestHelper.TUESDAY_SESSION,
+                    _serviceViewModelTestHelper.WEDNESDAY_SESSION,
+                    _serviceViewModelTestHelper.THURSDAY_MORNING_SESSION,
+                    _serviceViewModelTestHelper.FRIDAY_SESSION
+                }
+            };
+
+            var todaysOpeningTimes = service.NextOpenDayRotaSessions;
+            Assert.AreEqual(1, todaysOpeningTimes.Count());
+            Assert.AreEqual(DayOfWeek.Friday, todaysOpeningTimes.First().Day);
+            Assert.IsTrue(_serviceViewModelTestHelper.IsSameTime(todaysOpeningTimes.First().OpeningTime, _serviceViewModelTestHelper.FRIDAY_SESSION.StartTime));
+            Assert.IsTrue(_serviceViewModelTestHelper.IsSameTime(todaysOpeningTimes.First().ClosingTime, _serviceViewModelTestHelper.FRIDAY_SESSION.EndTime));
+
         }
     }
 }
