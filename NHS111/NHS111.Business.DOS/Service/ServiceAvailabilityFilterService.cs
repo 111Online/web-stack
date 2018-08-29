9fdf6014c9fa7f27f068ffcf78db80332ed3959c
Patrick Hughes <patrick.hughes1@nhs.net>
2018-08-29 17:04:31 +0100

    added json convertor to pass search time to dosservice

--- NHS111/NHS111.Business.DOS/Service/ServiceAvailabilityFilterService.cs
+++ NHS111/NHS111.Business.DOS/Service/ServiceAvailabilityFilterService.cs
@@ -13,6 +13,7 @@ using NHS111.Business.DOS.WhitelistFilter;
 using NHS111.Models.Models.Web.DosRequests;
 using NHS111.Features;
 using NHS111.Models.Models.Business;
+using NHS111.Models.Models.Web.Clock;
 
 namespace NHS111.Business.DOS.Service
 {
@@ -60,7 +61,16 @@ namespace NHS111.Business.DOS.Service
             var val = await response.Content.ReadAsStringAsync();
             var jObj = (JObject)JsonConvert.DeserializeObject(val);
             var services = jObj["CheckCapacitySummaryResult"];
-            var results = services.ToObject<List<Models.Models.Business.DosService>>();
+            
+            // get the search datetime if one has been set, if not set to now
+            DateTime searchDateTime;
+            if (!DateTime.TryParse(dosCase.SearchDateTime, out searchDateTime)) 
+                searchDateTime = DateTime.Now;
+
+            // use dosserviceconvertor to specify the time to use for each dos service object
+            var settings = new JsonSerializer();
+            settings.Converters.Add(new DosServiceConverter(new SearchDateTimeClock(searchDateTime)));
+            var results = services.ToObject<IList<Models.Models.Business.DosService>>(settings);
 
             var publicHolidayAjustedResults =
                 _publicHolidayService.AdjustServiceRotaSessionOpeningForPublicHoliday(results);
@@ -117,5 +127,51 @@ namespace NHS111.Business.DOS.Service
         {
             get { return _checkCapacitySummaryResults; }
         }
+    }
+
+//TODO:: Put these in the right places VVVVVVV
+    public class DosServiceConverter : JsonConverter
+    {
+        private readonly IClock _clock;
+
+        public DosServiceConverter(IClock clock)
+        {
+            _clock = clock;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            // Load the JSON for the Result into a JArray
+            var servicesArray = JArray.Load(reader);
+            var dosServices = new List<Models.Models.Business.DosService>();
+            foreach (var service in servicesArray)
+            {
+                var dosService = new Models.Models.Business.DosService(_clock);
+                serializer.Populate(service.CreateReader(), dosService);
+                dosServices.Add(dosService);
+            }
+
+            // Return the result
+            return dosServices;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(IList<Models.Models.Business.DosService>));
+        }
+    }
+
+    public class SearchDateTimeClock : IClock
+    {
+        public SearchDateTimeClock(DateTime searchDatetime)
+        {
+            Now = searchDatetime;
+        }
+        public DateTime Now { get; }
     }
 }
