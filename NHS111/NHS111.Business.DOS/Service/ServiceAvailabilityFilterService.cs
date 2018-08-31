ade5c8b75916fa932b280c3b988d2fd7d2eb35de
Patrick Hughes <patrick.hughes1@nhs.net>
2018-08-31 10:47:17 +0100

    refactored json converter code

--- NHS111/NHS111.Business.DOS/Service/ServiceAvailabilityFilterService.cs
+++ NHS111/NHS111.Business.DOS/Service/ServiceAvailabilityFilterService.cs
@@ -14,6 +14,7 @@ using NHS111.Models.Models.Web.DosRequests;
 using NHS111.Features;
 using NHS111.Models.Models.Business;
 using NHS111.Models.Models.Web.Clock;
+using NHS111.Utils.Converters;
 
 namespace NHS111.Business.DOS.Service
 {
@@ -126,58 +127,6 @@ namespace NHS111.Business.DOS.Service
         public CheckCapacitySummaryResult[] CheckCapacitySummaryResults
         {
             get { return _checkCapacitySummaryResults; }
-        }
-    }
-
-//TODO:: Put these in the right places VVVVVVV
-    public class DosServiceConverter : JsonConverter
-    {
-        private readonly IClock _clock;
-
-        public DosServiceConverter(IClock clock)
-        {
-            _clock = clock;
-        }
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            // Load the JSON for the Result into a JArray
-            var servicesArray = JArray.Load(reader);
-            var dosServices = new List<Models.Models.Business.DosService>();
-            foreach (var service in servicesArray)
-            {
-                var dosService = new Models.Models.Business.DosService(_clock);
-                serializer.Populate(service.CreateReader(), dosService);
-                dosServices.Add(dosService);
-            }
-
-            // Return the result
-            return dosServices;
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            return (objectType == typeof(IList<Models.Models.Business.DosService>));
-        }
-    }
-
-    public class SearchDateTimeClock : IClock
-    {
-        private readonly DateTime _searchDatetime;
-
-        public SearchDateTimeClock(DateTime searchDatetime)
-        {
-            _searchDatetime = searchDatetime;
-        }
-
-        public DateTime Now
-        {
-            get { return _searchDatetime; }
         }
     }
 }
