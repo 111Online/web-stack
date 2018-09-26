c47da73eae9e059a5530168df6c6fc32659b6967
Shane Hudson <shane@shanehudson.net>
2018-09-26 14:00:51 +0100

    Adding in the Adobe Analytics feature toggle to use the framework correctly.

---
+++ NHS111/NHS111.Features/AdobeAnalyticsFeature.cs
@@ -0,0 +1,16 @@
+﻿using NHS111.Features.Defaults;
+
+namespace NHS111.Features {
+    public interface IAdobeAnalyticsFeature
+    : IFeature {
+
+    }
+
+    public class AdobeAnalyticsFeature
+        : BaseFeature, IAdobeAnalyticsFeature {
+
+        public AdobeAnalyticsFeature() {
+            DefaultIsEnabledSettingStrategy = new EnabledByDefaultSettingStrategy();
+        }
+    }
+}
