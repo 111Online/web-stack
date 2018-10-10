5496d03fa66a658e6896d6b1b5a7f507929d8412
Patrick <patrick.hughes1@nhs.net>
2018-10-10 12:25:04 +0100

    simplified test json

--- NHS111/NHS111.Models.Test/Models/Web/FromExternalServices/DosServiceTests.cs
+++ NHS111/NHS111.Models.Test/Models/Web/FromExternalServices/DosServiceTests.cs
@@ -24,7 +24,7 @@ namespace NHS111.Models.Test.Models.Web.FromExternalServices
         [Test]
         public void Empty_PublicName_Returns_Name()
         {
-            var json = "{\"idField\":1315835856,\"capacityField\":0,\"nameField\":\"ED - Emergency Department Stoke Mandeville Hospital, Bucks (CATCH ALL)\",\"publicNameField\":\"\",\"contactDetailsField\":\"01296 315664\",\"addressField\":\"Mandeville Road, Aylesbury, Bucks, HP21 8AL\",\"postcodeField\":\"HP21 8AL\",\"northingsField\":211849,\"northingsFieldSpecified\":true,\"eastingsField\":482858,\"eastingsFieldSpecified\":true,\"urlField\":\"\",\"serviceTypeField\":{\"idField\":40,\"nameField\":\"Emergency Department (ED)\",\"PropertyChanged\":null},\"odsCodeField\":\"RXQ02\",\"nonPublicTelephoneNoField\":\"\",\"faxField\":\"\",\"referralTextField\":\"You can go straight to this service. You do not need to telephone beforehand.|\",\"distanceField\":\"0.0\",\"notesField\":\"Referral Information: For minor injuries consider Wycombe MIIU.\",\"openAllHoursField\":true,\"waitTimesField\":{\"currentWaitTimeField\":\"\",\"patientsInDepartmentField\":\"\",\"patientsBeingSeenField\":\"\",\"notesField\":\"\",\"PropertyChanged\":null},\"rotaSessionsField\":[],\"openTimeSpecifiedSessionsField\":[],\"PropertyChanged\":null}";
+            var json = "{\"nameField\":\"ED - Emergency Department Stoke Mandeville Hospital, Bucks (CATCH ALL)\",\"publicNameField\":\"\"}";
             var service = JsonConvert.DeserializeObject<DosService>(json);
             Assert.AreEqual("ED - Emergency Department Stoke Mandeville Hospital, Bucks (CATCH ALL)", service.PublicName);
         }
@@ -32,7 +32,7 @@ namespace NHS111.Models.Test.Models.Web.FromExternalServices
         [Test]
         public void Value_In_PublicName_Returns_PublicName()
         {
-            var json = "{\"idField\":1315835856,\"capacityField\":0,\"nameField\":\"ED - Emergency Department Stoke Mandeville Hospital, Bucks (CATCH ALL)\",\"publicNameField\":\"Stoke Madeville Hospital\",\"contactDetailsField\":\"01296 315664\",\"addressField\":\"Mandeville Road, Aylesbury, Bucks, HP21 8AL\",\"postcodeField\":\"HP21 8AL\",\"northingsField\":211849,\"northingsFieldSpecified\":true,\"eastingsField\":482858,\"eastingsFieldSpecified\":true,\"urlField\":\"\",\"serviceTypeField\":{\"idField\":40,\"nameField\":\"Emergency Department (ED)\",\"PropertyChanged\":null},\"odsCodeField\":\"RXQ02\",\"nonPublicTelephoneNoField\":\"\",\"faxField\":\"\",\"referralTextField\":\"You can go straight to this service. You do not need to telephone beforehand.|\",\"distanceField\":\"0.0\",\"notesField\":\"Referral Information: For minor injuries consider Wycombe MIIU.\",\"openAllHoursField\":true,\"waitTimesField\":{\"currentWaitTimeField\":\"\",\"patientsInDepartmentField\":\"\",\"patientsBeingSeenField\":\"\",\"notesField\":\"\",\"PropertyChanged\":null},\"rotaSessionsField\":[],\"openTimeSpecifiedSessionsField\":[],\"PropertyChanged\":null}";
+            var json = "{\"nameField\":\"ED - Emergency Department Stoke Mandeville Hospital, Bucks (CATCH ALL)\",\"publicNameField\":\"Stoke Madeville Hospital\"}";
             var service = JsonConvert.DeserializeObject<DosService>(json);
             Assert.AreEqual("Stoke Madeville Hospital", service.PublicName);
         }
