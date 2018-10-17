7f592f536fbf9b54dca7515e5ecb5c373246ea42
Adam Naylor <adamnaylor@gmail.com>
2018-10-17 16:04:33 +0100

    Adds dxcode remapping

--- NHS111/NHS111.Models/Mappers/WebMappings/FromOutcomeViewModelToDosViewModel.cs
+++ NHS111/NHS111.Models/Mappers/WebMappings/FromOutcomeViewModelToDosViewModel.cs
@@ -6,6 +6,7 @@ using NHS111.Models.Models.Web;
 
 namespace NHS111.Models.Mappers.WebMappings
 {
+    using System.Configuration;
     using System.Linq;
 
     public class FromOutcomeViewModelToDosViewModel : Profile
@@ -28,7 +29,7 @@ namespace NHS111.Models.Mappers.WebMappings
                 .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.SessionId))
                 .ForMember(dest => dest.PostCode, opt => opt.MapFrom(src => src.CurrentPostcode))
                 .ForMember(dest => dest.Disposition,
-                    opt => opt.ResolveUsing<DispositionResolver>().FromMember(src => src.Id))
+                            opt => opt.ResolveUsing<DispositionResolver>().FromMember(src => src.Id))
                 .ForMember(dest => dest.SymptomDiscriminatorList,
                     opt =>
                         opt.ResolveUsing<SymptomDiscriminatorListResolver>()
@@ -52,8 +53,9 @@ namespace NHS111.Models.Mappers.WebMappings
 
         public class DispositionResolver : ValueResolver<string, int>
         {
-            protected override int ResolveCore(string source)
-            {
+            protected override int ResolveCore(string source) {
+                source = Remap(source);
+
                 if (!source.StartsWith("Dx")) throw new FormatException("Dx code does not have prefix \"Dx\". Cannot convert");
                 var code = source.Replace("Dx", "");
                 if (code.Length == 3)
@@ -65,6 +67,24 @@ namespace NHS111.Models.Mappers.WebMappings
                 }
                 
                 return Convert.ToInt32("10" + code);
+            }
+
+            private static string Remap(string source) {
+                var mappingsForDx333 = ConfigurationManager.AppSettings["DxCodeMappingsForDx333"];
+                if (mappingsForDx333 != null) {
+                    var remapped333Codes = mappingsForDx333.Split(',');
+                    if (remapped333Codes.Contains(source))
+                        source = "Dx333";
+                }
+
+                var mappingsForDx334 = ConfigurationManager.AppSettings["DxCodeMappingsForDx334"];
+                if (mappingsForDx334 != null) {
+                    var remapped334Codes = mappingsForDx334.Split(',');
+                    if (remapped334Codes.Contains(source))
+                        source = "Dx334";
+                }
+
+                return source;
             }
         }
 
