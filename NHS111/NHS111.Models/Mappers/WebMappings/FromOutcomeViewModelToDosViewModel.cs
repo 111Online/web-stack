f529d74f4dc7011c369316c54561ca403aa19613
Adam Naylor <adamnaylor@gmail.com>
2018-10-29 09:52:50 +0000

    Adds callback and redirect (to perrsonal details) to 999

--- NHS111/NHS111.Models/Mappers/WebMappings/FromOutcomeViewModelToDosViewModel.cs
+++ NHS111/NHS111.Models/Mappers/WebMappings/FromOutcomeViewModelToDosViewModel.cs
@@ -53,7 +53,12 @@ namespace NHS111.Models.Mappers.WebMappings
 
         public class DispositionResolver : ValueResolver<string, int>
         {
+<<<<<<< HEAD
             protected override int ResolveCore(string source) {
+=======
+            protected override int ResolveCore(string source)
+            {
+>>>>>>> Adds callback and redirect (to perrsonal details) to 999
                 source = Remap(source);
 
                 if (!source.StartsWith("Dx")) throw new FormatException("Dx code does not have prefix \"Dx\". Cannot convert");
@@ -69,16 +74,29 @@ namespace NHS111.Models.Mappers.WebMappings
                 return Convert.ToInt32("10" + code);
             }
 
+<<<<<<< HEAD
             private static string Remap(string source) {
                 var mappingsForDx333 = ConfigurationManager.AppSettings["DxCodeMappingsForDx333"];
                 if (mappingsForDx333 != null) {
+=======
+            private static string Remap(string source)
+            {
+                var mappingsForDx333 = ConfigurationManager.AppSettings["DxCodeMappingsForDx333"];
+                if (mappingsForDx333 != null)
+                {
+>>>>>>> Adds callback and redirect (to perrsonal details) to 999
                     var remapped333Codes = mappingsForDx333.Split(',');
                     if (remapped333Codes.Contains(source))
                         source = "Dx333";
                 }
 
                 var mappingsForDx334 = ConfigurationManager.AppSettings["DxCodeMappingsForDx334"];
+<<<<<<< HEAD
                 if (mappingsForDx334 != null) {
+=======
+                if (mappingsForDx334 != null)
+                {
+>>>>>>> Adds callback and redirect (to perrsonal details) to 999
                     var remapped334Codes = mappingsForDx334.Split(',');
                     if (remapped334Codes.Contains(source))
                         source = "Dx334";
@@ -86,6 +104,10 @@ namespace NHS111.Models.Mappers.WebMappings
 
                 return source;
             }
+<<<<<<< HEAD
+=======
+
+>>>>>>> Adds callback and redirect (to perrsonal details) to 999
         }
 
 
