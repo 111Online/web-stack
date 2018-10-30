99faacfdd81a742c104f806d869f4ebadd1d46fa
Adam Naylor <adamnaylor@gmail.com>
2018-10-30 15:17:20 +0000

    Adds 999 callback confirmation screen

---
+++ NHS111/NHS111.DOS.Functional.Tests/Call999CallbackTests.cs
@@ -0,0 +1,40 @@
+﻿namespace NHS111.DOS.Functional.Tests {
+    using NUnit.Framework;
+    using SmokeTest.Utils;
+
+    [TestFixture()]
+    public class Call999CallbackTests
+    : BaseTests {
+
+        [TestCase(OutcomePage.Cat3999Text)] //callback returned
+        //[TestCase(OutcomePage.Call999CallbackText)] //no callback returned
+        public void Call999Cat3_WithDosResult_DisplaysExpectedDispositionPage(string expectedOutcomeText) {
+            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Headache", TestScenerioSex.Male, TestScenerioAgeGroups.Adult);
+
+            var outcomePage = questionPage
+                .Answer(1)
+                .Answer(1)
+                .Answer(3)
+                .Answer(1)
+                .AnswerForDeadEnd<OutcomePage>("Yes");
+
+            outcomePage.VerifyOutcome(expectedOutcomeText);
+        }
+
+        [TestCase(OutcomePage.Cat4999Text)] //callback returned
+        //[TestCase(OutcomePage.Call999CallbackText)] //no callback returned
+        public void Call999Cat4_WithDosResult_DisplaysExpectedDispositionPage(string expectedOutcomeText)
+        {
+            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Finger or Thumb Injury, Penetrating", TestScenerioSex.Male, TestScenerioAgeGroups.Adult);
+
+            var outcomePage = questionPage
+                .Answer(1)
+                .Answer(3)
+                .Answer(1)
+                .AnswerSuccessiveByOrder(3, 5)
+                .AnswerForDeadEnd<OutcomePage>("No");
+
+            outcomePage.VerifyOutcome(expectedOutcomeText);
+        }
+    }
+}
