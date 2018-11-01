6416895387aa4def963e84651d1bd39a560c678c
taxworthy <tom.axworthy@nhs.net>
2018-11-01 14:54:07 +0000

    Add page not found tests (404)

---
+++ NHS111/NHS111.SmokeTest.Utils/PageNotFound.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+using System.Security;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace NHS111.SmokeTest.Utils
+{
+    public class PageNotFound : LayoutPage
+    {
+        private static string _errorUrl = ConfigurationManager.AppSettings["TestWebsiteUrl"] + "/this_should_not_exist";
+
+        private static string _errorTitleCode = "Error 404";
+        private static string _errorTitle = "Sorry, there is a problem.";
+
+        [FindsBy(How = How.CssSelector, Using = "main[id='content'] > h1")]
+        private static IWebElement PageTitle { get; set; }
+
+        public PageNotFound(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void Load()
+        {
+            Driver.Navigate().GoToUrl(_errorUrl);
+        }
+
+        public void Verify()
+        {
+            Assert.IsTrue(PageTitle.Displayed);
+            Assert.AreEqual(_errorTitle,PageTitle.Text);
+            Assert.IsTrue(Driver.Title.Contains(_errorTitleCode));
+        }
+    }
+}
---
+++ NHS111/NHS111.SmokeTests/PageNotFoundTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using NHS111.SmokeTest.Utils;
+
+namespace NHS111.SmokeTests
+{
+    [TestFixture]
+    public class PageNotFoundTests : BaseTests
+    {
+        [Test]
+        public void PageNotFound_Displays()
+        {
+            TestScenarioPart.HomePage(Driver);
+
+            var pageNotFound = TestScenarioPart.PageNotFound(Driver);
+            
+            pageNotFound.Verify();
+        }
+    }
+}
