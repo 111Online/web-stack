8b1f39ec5bb60194c400c3dcb1718f158f62edcb
Adam Naylor <adamnaylor@gmail.com>
2018-11-07 10:14:58 +0000

    Adds functional tests coverage

---
+++ NHS111/NHS111.DOS.Functional.Tests/EDValidationTests.cs
@@ -0,0 +1,104 @@
+﻿
+namespace NHS111.DOS.Functional.Tests {
+    using NUnit.Framework;
+    using SmokeTest.Utils;
+    using SmokeTests;
+
+    [TestFixture(Description = "Tests the callback/validation flow for Emergency Department outcomes.")]
+    public class EDValidationTests
+        : BaseTests {
+
+        [Test]
+        [Ignore("Currently all ED dxcodes are remapped")]
+        public void EDOutcome_NotMappedToDx334_ShowsOriginalOutcome() {
+            var edOutcome = NavigateToNonRemappedEDOutcome();
+            AssertIsOriginalOutcome(edOutcome);
+        }
+
+        [Test] //requires NO callback services returned for this scenario
+        public void EDOutcome_MappedToDx334ButNoCallbackServicesReturned_ShowsOriginalOutcome() {
+            var edOutcome = NavigateToRemappedEDOutcome();
+            AssertIsOriginalOutcome(edOutcome);
+        }
+
+        [Test]
+        public void EDOutcome_MappedToDx334AndReturnsCallbackServices_ShowsCallbackAcceptancePage() {
+            var callbackAcceptancePage = NavigateToRemappedEDOutcome();
+            AssertIsCallbackAcceptancePage(callbackAcceptancePage);
+        }
+
+        [Test]
+        public void EDOutcome_WhenUserRejectsCallbackOffer_ShowsOriginalOutcome() {
+            var callbackAcceptancePage = NavigateToRemappedEDOutcome();
+            var edOutcome = RejectCallback(callbackAcceptancePage);
+            AssertIsOriginalOutcome(edOutcome);
+        }
+
+        [Test]
+        public void EDOutcome_WhenUserAcceptsCallbackOffer_ShowsPersonalDetailsPage() {
+            var callbackAcceptancePage = NavigateToRemappedEDOutcome();
+            var edOutcome = AcceptCallback(callbackAcceptancePage);
+            AssertIsPersonalDetailsPage(edOutcome);
+        }
+
+        [Test]
+        public void EDOutcome_WhenNoPostcodePresent_ShowsPostcodePageAndThenPersonalDetailsPage() {
+            var postcodePage = NavigateToRemappedEDOutcome();
+            AssertIsPostcodePage(postcodePage);
+            var callbackAcceptancePage = EnterPostcode("LS17 7NZ", postcodePage);
+            AssertIsCallbackAcceptancePage(callbackAcceptancePage);
+            var edOutcome = AcceptCallback(callbackAcceptancePage);
+            AssertIsPersonalDetailsPage(edOutcome);
+        }
+
+        private OutcomePage RejectCallback(OutcomePage callbackAcceptancePage) {
+            throw new System.NotImplementedException();
+        }
+
+        private OutcomePage AcceptCallback(OutcomePage callbackAcceptancePage) {
+            throw new System.NotImplementedException();
+        }
+
+        private OutcomePage EnterPostcode(string postcode, OutcomePage postcodePage) {
+            throw new System.NotImplementedException();
+        }
+
+        private OutcomePage NavigateToNonRemappedEDOutcome() {
+            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Headache", TestScenerioSex.Male,
+                TestScenerioAgeGroups.Adult);
+
+            return questionPage
+                .AnswerSuccessiveByOrder(3, 3)
+                .Answer(5)
+                .AnswerSuccessiveByOrder(3, 3)
+                .AnswerForDispostion<OutcomePage>("Yes");
+        }
+
+        private OutcomePage NavigateToRemappedEDOutcome() {
+            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Headache", TestScenerioSex.Male,
+                TestScenerioAgeGroups.Adult);
+
+            return questionPage
+                .AnswerSuccessiveByOrder(3, 3)
+                .Answer(5)
+                .AnswerSuccessiveByOrder(3, 3)
+                .AnswerForDispostion<OutcomePage>("Yes");
+        }
+
+        private void AssertIsOriginalOutcome(OutcomePage edOutcome) {
+            edOutcome.VerifyDispositionCode("Dx02");
+        }
+
+        private void AssertIsPersonalDetailsPage(OutcomePage edOutcome) {
+            Assert.True(false);
+        }
+
+        private void AssertIsPostcodePage(OutcomePage edOutcome) {
+            Assert.True(false);
+        }
+
+        private void AssertIsCallbackAcceptancePage(OutcomePage edOutcome) {
+            Assert.True(false);
+        }
+    }
+}
--- NHS111/NHS111.SmokeTests/EndpointJourneyTests.cs
+++ NHS111/NHS111.SmokeTests/EndpointJourneyTests.cs
@@ -1,13 +1,31 @@
-﻿using NHS111.SmokeTest.Utils;
-using NUnit.Framework;
-
 namespace NHS111.SmokeTests
 {
+    using System.Linq;
+    using SmokeTest.Utils;
+    using NUnit.Framework;
     using OpenQA.Selenium;
 
     [TestFixture]
-    public class EndpointJourneyTests : BaseTests
-    {
+    public class EndpointJourneyTests
+        : BaseTests {
+
+        [TestCase("Male", 22, "Headache", new[] {3, 3, 3, 5, 3, 3, 3, 1}, "Dx02", TestName = "Can reach Dx02")]
+        [TestCase("Male", 24, "Sexual Concerns", new[] {3, 4, 3, 3, 3, 4, 4, 1, 1, 3}, "Dx03", TestName = "Can reach Dx03")]
+        [TestCase("Male", 6, "Object, Ingested or Inhaled", new[] {1, 3, 3, 5, 3, 5, 3, 3, 3, 3, 3, 3, 3}, "Dx89", TestName = "Can reach Dx89")]
+        [TestCase("Female", 16, "Mental Health Problems", new[] {1, 5, 3, 5, 3, 1, 4}, "Dx92", TestName = "Can reach Dx92")]
+        [TestCase("Female", 22, "Sexual or Menstrual Concerns", new[] {1}, "Dx94", TestName = "Can reach Dx94")]
+        public void TestOutcomes(string sex, int age, string pathwayTitle, int[] answers, string expectedDxCode) {
+            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, pathwayTitle, sex, age);
+
+            for (var i = 0; i < answers.Length - 1; i++) {
+                questionPage.Answer(answers[i]);
+            }
+
+            var outcomePage = questionPage.AnswerForDispostion<OutcomePage>(answers.Last());
+            //take screenshot
+            outcomePage.VerifyDispositionCode(expectedDxCode);
+        }
+
         [Test]
         public void Call999EndpointJourney()
         {
