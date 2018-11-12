97b83798a532fa9550233d9dc6a08daf05411102
taxworthy <tom.axworthy@nhs.net>
2018-11-12 12:20:01 +0000

    Fix changes for 00 at start of telephone number

--- NHS111/NHS111.Models/Models/Web/UserInfo.cs
+++ NHS111/NHS111.Models/Models/Web/UserInfo.cs
@@ -54,11 +54,11 @@ namespace NHS111.Models.Models.Web
 
         private String RemoveValidInternationalPrefix(string telephoneNumber)
         {
-            if (telephoneNumber.LastIndexOf("00", StringComparison.Ordinal) == 0)
-                telephoneNumber = telephoneNumber.Replace("00", "+");
+            if (telephoneNumber.StartsWith("00"))
+                telephoneNumber = "+" + telephoneNumber.Substring(2);
 
-            if (telephoneNumber.LastIndexOf("+44", StringComparison.Ordinal) == 0)
-                telephoneNumber = telephoneNumber.Replace("+44", "0");
+            if (telephoneNumber.StartsWith("+44"))
+                telephoneNumber = "0" + telephoneNumber.Substring(3);
 
             return telephoneNumber;
         }
