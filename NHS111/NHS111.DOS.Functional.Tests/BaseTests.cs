8d7225dedfbd032590b2827bc61d70c177557535
Adam Naylor <adamnaylor@gmail.com>
2018-11-27 16:47:51 +0000

    Amends advice to visit A&E, also adds correct timeframe.

--- NHS111/NHS111.DOS.Functional.Tests/BaseTests.cs
+++ NHS111/NHS111.DOS.Functional.Tests/BaseTests.cs
@@ -1,28 +1,33 @@
 ﻿namespace NHS111.DOS.Functional.Tests {
     using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Threading.Tasks;
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
 
-    public class BaseTests
-    {
+    public class BaseTests {
         public IWebDriver Driver;
 
-        public BaseTests()
-        {
+        public BaseTests() {
             Driver = new ChromeDriver();
         }
 
-        ~BaseTests()
-        {
-            try
-            {
+        ~BaseTests() {
+            try {
                 Driver.Quit();
                 Driver.Dispose();
             }
-            catch (Exception)
-            {
+            catch (Exception) {
                 // Ignore errors if unable to close the browser
             }
+        }
+
+        public void SaveScreenAsPNG(string filename) {
+            var screenshotDriver = Driver as ITakesScreenshot;
+            var screenshot = screenshotDriver.GetScreenshot();
+            screenshot.SaveAsFile(Path.Combine("C:\\", filename + ".png"));
         }
     }
 }
