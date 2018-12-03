667e039d6b912bc9eed2da9eea55f532250b693c
taxworthy <tom.axworthy@nhs.net>
2018-12-03 09:16:28 +0000

    Create category for tests to not be run in nightly build, rename test projects

---
+++ NHS111/NHS111.Web.Functional.PreRelease.Tests/PageNotFoundTests.cs
@@ -0,0 +1,20 @@
+﻿using NHS111.Web.Functional.Utils;
+using NUnit.Framework;
+
+namespace NHS111.Web.Functional.PreRelease.Tests
+{
+    [TestFixture]
+    public class PageNotFoundTests : BaseTests
+    {
+        [Test]
+        public void PageNotFound_Displays()
+        {
+            TestScenarioPart.HomePage(Driver);
+
+
+            var pageNotFound = TestScenarioPart.PageNotFound(Driver);
+            
+            pageNotFound.Verify();
+        }
+    }
+}
---
+++ NHS111/NHS111.Web.Functional.Tests/QuestionPageTests.cs
@@ -0,0 +1,38 @@
+﻿using NHS111.Web.Functional.Utils;
+using NUnit.Framework;
+
+namespace NHS111.Web.Functional.Tests
+{
+    [TestFixture]
+    public class QuestionPageTests : BaseTests
+    {
+        [Test]
+        public void QuestionPage_DisplaysPage_And_Rationale()
+        {
+            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Headache", TestScenerioSex.Female, TestScenerioAgeGroups.Adult);
+
+            questionPage.Verify();
+            questionPage.VerifyRationale();
+        }
+
+        [Test]
+        public void QuestionPage_DisplaysPreviousQuestionLink_OnSecondQuestion()
+        {
+            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Headache", TestScenerioSex.Female, TestScenerioAgeGroups.Adult);
+            questionPage.VerifyPreviousButton(true);
+
+            var secondQuestion = questionPage.AnswerYes();
+            secondQuestion.VerifyPreviousButton();
+        }
+
+        [Test]
+        public void QuestionPage_DisplaysQuestionAdditionalInfo()
+        {
+            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Skin Problems", TestScenerioSex.Female, TestScenerioAgeGroups.Adult);
+
+            questionPage.VerifyAdditionalInfo();
+        }
+
+
+    }
+}
