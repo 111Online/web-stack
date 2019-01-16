4da91ac808badb9b5feab5409fe31675280946af
Patrick <patrick.hughes1@nhs.net>
2019-01-16 18:46:08 +0000

    added gate specific home page tests

---
+++ NHS111/NHS111.Web.Functional.Utils/Expert24Page.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+
+namespace NHS111.Web.Functional.Utils
+{
+    public class Expert24Page: LayoutPage, ISubmitPostcodeResult
+    {
+
+        public Expert24Page(IWebDriver driver)
+            : base(driver) { }
+
+        public bool ValidationVisible()
+        {
+            return false;
+        }
+    }
+}
