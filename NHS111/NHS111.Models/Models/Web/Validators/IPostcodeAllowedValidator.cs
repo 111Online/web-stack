246b85ae81fd9696d3debcf231c7019599e77b1a
Patrick <patrick.hughes1@nhs.net>
2019-01-16 13:40:49 +0000

    migrated controllers, buisiness logic and views from gate

--- NHS111/NHS111.Models/Models/Web/Validators/IPostcodeAllowedValidator.cs
+++ NHS111/NHS111.Models/Models/Web/Validators/IPostcodeAllowedValidator.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NHS111.Models.Models.Web.CCG;
 
 namespace NHS111.Models.Models.Web.Validators
 {
     public interface IPostCodeAllowedValidator
     {
         PostcodeValidatorResponse IsAllowedPostcode(string postcode);
+        CCGModel CcgModel { get; }
     }
 
 
