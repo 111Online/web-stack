8847060873ed68e8ad027e496786ac9f8f67c9d9
Patrick Hughes <patrick.hughes1@nhs.net>
2019-01-17 16:52:36 +0000

    send stp and ccg data to google analytics

--- NHS111/NHS111.Utils/Analytics/EventMeasurement.cs
+++ NHS111/NHS111.Utils/Analytics/EventMeasurement.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Net.Http;
+using NHS111.Utils.Filters;
 
 namespace NHS111.Utils.Analytics
 {
     public class EventMeasurement : MeasurementProtocol
     {
+        public EventMeasurement(string trackingId, string clientId, string eventCategory) : base(trackingId)
+        {
+            EventCategory = eventCategory;
+            ClientId = clientId;
+        }
 
         public string EventCategory { get; set; }
 
         public string EventAction { get; set; }
 
         public string EventLabel { get; set; }
+        public string Campaign { get; set; }
+        public string Source { get; set; }
 
         public override string Payload
         {
-            get { return string.Format("v={0}&t=event&tid={1}&cid={2}&uid={3}&ec={4}&ea={5}&el={6}", Version, TrackingId, ClientId, UserId, EventCategory, EventAction, EventLabel); }
+            get { return string.Format("v={0}&t=event&tid={1}&cid={2}&uid={3}&ec={4}&ea={5}&el={6}&cn={7}&cs={8}", Version, TrackingId, ClientId, UserId, EventCategory, EventAction, EventLabel, Campaign, Source); }
         }
 
         public override async void PostToAnalyticsAsync(string requestUri)
