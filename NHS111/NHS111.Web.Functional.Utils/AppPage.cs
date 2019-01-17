9ac7494810104cf0f190383657e6ea2e87fc817c
Patrick Hughes <patrick.hughes1@nhs.net>
2019-01-17 13:55:13 +0000

    merge

--- NHS111/NHS111.Web.Functional.Utils/AppPage.cs
+++ NHS111/NHS111.Web.Functional.Utils/AppPage.cs
@@ -12,7 +12,10 @@ namespace NHS111.Web.Functional.Utils
         [FindsBy(How = How.TagName, Using = "h2")]
         private IWebElement AppHeader { get; set; }
 
-        public string AppName => AppHeader.Text;
+        public string AppName
+        {
+            get { return AppHeader.Text; }
+        }
 
         public bool ValidationVisible()
         {
