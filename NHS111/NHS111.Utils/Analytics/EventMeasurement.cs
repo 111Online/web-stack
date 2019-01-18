8ca62420c127b9b145b1bf89c768424d8ea4f005
Patrick <patrick.hughes1@nhs.net>
2019-01-18 12:05:59 +0000

    send campaign and source pageview data to GA

--- NHS111/NHS111.Utils/Analytics/EventMeasurement.cs
+++ NHS111/NHS111.Utils/Analytics/EventMeasurement.cs
@@ -6,23 +6,34 @@ namespace NHS111.Utils.Analytics
 {
     public class EventMeasurement : MeasurementProtocol
     {
-        public EventMeasurement(string trackingId, string clientId, string eventCategory) : base(trackingId)
+        public EventMeasurement(string trackingId, string clientId, HitType hitType) : base(trackingId, hitType)
         {
-            EventCategory = eventCategory;
             ClientId = clientId;
         }
 
         public string EventCategory { get; set; }
-
         public string EventAction { get; set; }
-
         public string EventLabel { get; set; }
         public string Campaign { get; set; }
         public string Source { get; set; }
-
+        public string DocumentPath { get; set; }
         public override string Payload
         {
-            get { return string.Format("v={0}&t=event&tid={1}&cid={2}&uid={3}&ec={4}&ea={5}&el={6}&cn={7}&cs={8}", Version, TrackingId, ClientId, UserId, EventCategory, EventAction, EventLabel, Campaign, Source); }
+            get { return GetPayload(); }
+        }
+
+        private string GetPayload()
+        {
+            switch (HitType)
+            {
+                case HitType.PageView:
+                    return string.Format("v={0}&t=pageview&tid={1}&cid={2}&uid={3}&cn={4}&cs={5}&dp={6}&cm=Direct", Version, TrackingId, ClientId, UserId, Campaign, Source, DocumentPath);
+                case HitType.Event:
+                    return string.Format("v={0}&t=event&tid={1}&cid={2}&uid={3}&ec={4}&ea={5}&el={6}", Version, TrackingId, ClientId, UserId, EventCategory, EventAction, EventLabel);
+                default:
+                    throw new ArgumentOutOfRangeException(string.Format("A measurement protocol has not been defined for hit type {0}", HitType));
+            }
+            
         }
 
         public override async void PostToAnalyticsAsync(string requestUri)
