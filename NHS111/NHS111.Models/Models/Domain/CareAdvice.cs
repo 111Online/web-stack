1117f8f337d2055fe0837aedf5e7b8bddf3c9cdd
Patrick <patrick.hughes1@nhs.net>
2019-01-23 07:47:05 +0000

    fix missing refs

--- NHS111/NHS111.Models/Models/Domain/CareAdvice.cs
+++ NHS111/NHS111.Models/Models/Domain/CareAdvice.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Newtonsoft.Json;
 
 namespace NHS111.Models.Models.Domain
 {
+    using System.Diagnostics;
+
+    [DebuggerDisplay("{Id}: {Keyword} ({Items.Count})")]
     public class CareAdvice
     {
         public CareAdvice() { }
