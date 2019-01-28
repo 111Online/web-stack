f3e2409a7d46d1e8d6bd3cd4beb2746399ebfcbc
Shane Hudson <shane@shanehudson.net>
2019-01-28 15:24:06 +0000

    Updated feedback error message with p tag to match success.

--- NHS111/NHS111.Models/Models/Web/FeedbackConfirmation.cs
+++ NHS111/NHS111.Models/Models/Web/FeedbackConfirmation.cs
@@ -5,7 +5,7 @@ namespace NHS111.Models.Models.Web
   public class FeedbackConfirmation
   {
         private static string _successMessage = @"<p>Thank you.</p><p>We cannot reply to any comments or pass them on to other NHS services.</p><p>To get medical help you should carry on answering the questions or call 111.</p> <p>If you'd like to give more feedback about using 111 online, there's a survey at the end of the questions.</p>";
-        private static string _errorMessage = "Sorry, there is a technical problem. Try again in a few moments.";
+        private static string _errorMessage = @"<p>Sorry, there is a technical problem. Try again in a few moments.</p>";
 
         [JsonProperty(PropertyName = "message")]
         public string Message { get; set; }
