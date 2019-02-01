8a50bce2adc56f7da5b3ed0db7bf360bf6c6008e
Patrick <patrick.hughes1@nhs.net>
2019-02-01 10:58:00 +0000

    aligned rules for setting date time with DOS and added tests

--- NHS111/NHS111.Features/IoC/FeatureRegistry.cs
+++ NHS111/NHS111.Features/IoC/FeatureRegistry.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NHS111.Models.Models.Web.Clock;
 using StructureMap;
 using StructureMap.Graph;
 
@@ -12,6 +8,7 @@ namespace NHS111.Features.IoC
     {
         public FeatureRegistry()
         {
+            For<IDOSSpecifyDispoTimeFeature>().Use(new DOSSpecifyDispoTimeFeature(new SystemClock()));
             Scan(scan =>
             {
                 scan.TheCallingAssembly();
