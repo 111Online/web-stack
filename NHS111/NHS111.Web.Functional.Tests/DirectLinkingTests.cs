74b84e20200abe46dbb7a673320ae355aab9c61c
Shane Hudson <shane@shanehudson.net>
2019-02-05 10:59:32 +0000

    Updated DoSSearchDateTime for direct linking tests to use 2020.

--- NHS111/NHS111.Web.Functional.Tests/DirectLinkingTests.cs
+++ NHS111/NHS111.Web.Functional.Tests/DirectLinkingTests.cs
@@ -15,7 +15,7 @@ namespace NHS111.Web.Functional.Tests
         
         [TestCase("/question/direct/PW755MaleAdult/24/Headache/ls177nz/?answers=2,2,2,4,0,1,0,2,2,2,0,2", "Dx05",  TestName = "Can reach Dx05")]
         [TestCase("/question/direct/PW1134MaleAdult/20/Eye,RedorIrritable/ls14br/?answers=2,2,1,2,2,2,2,2,2,3,0", "Dx28",  TestName = "Can reach Dx28")]
-        [TestCase("/question/direct/PW1610MaleAdult/25/Dentalproblems/ls177nz/?answers=1,2,0,0,0,0,2,2&dossearchdatetime=2018-09-21 22:30", "Dx19",  TestName = "DoS SearchTimeDate doesn't break direct link")]
+        [TestCase("/question/direct/PW1610MaleAdult/25/Dentalproblems/ls177nz/?answers=1,2,0,0,0,0,2,2&dossearchdatetime=2020-02-01 22:30", "Dx19",  TestName = "DoS SearchTimeDate doesn't break direct link")]
         public void CorrectPage(string path, string id)
         {
             var directLink = TestScenarioPart.DirectLinking(Driver, path);
@@ -26,12 +26,12 @@ namespace NHS111.Web.Functional.Tests
         public void DateTimeCorrect()
         {
             // Before 22:30 Dental Emergency UAT - Leeds, West Yorkshire (Unplanned Booking Service) should show
-            var firstTime = TestScenarioPart.DirectLinking(Driver, "/question/direct/PW620FemaleAdult/19/Dentalinjury/LS177NZ/?answers=2,4,0,0,0,2,0,0,2,0&dossearchdatetime=2019-02-28 20:30");
+            var firstTime = TestScenarioPart.DirectLinking(Driver, "/question/direct/PW620FemaleAdult/19/Dentalinjury/LS177NZ/?answers=2,4,0,0,0,2,0,0,2,0&dossearchdatetime=2020-02-01 20:30");
             firstTime.VerifyBookACall("1362471653");
 
             
             // After 22:30 NHS111 Dental Callback - Leeds should show
-            var secondTime = TestScenarioPart.DirectLinking(Driver, "/question/direct/PW620FemaleAdult/19/Dentalinjury/LS177NZ/?answers=2,4,0,0,0,2,0,0,2,0&dossearchdatetime=2019-02-28 00:00");
+            var secondTime = TestScenarioPart.DirectLinking(Driver, "/question/direct/PW620FemaleAdult/19/Dentalinjury/LS177NZ/?answers=2,4,0,0,0,2,0,0,2,0&dossearchdatetime=2020-02-01 00:00");
             secondTime.VerifyBookACall("1479808152");
         }
         
@@ -50,7 +50,7 @@ namespace NHS111.Web.Functional.Tests
             public void CorrectEDService()
             {
                 // ED Validation page shows with correct service
-                var directLink = TestScenarioPart.DirectLinking(Driver, "/question/direct/PW1685MaleAdult/24/Sexual Concerns/AL74HL/?answers=2,3,2,2,2,3,3,0,0,2&dossearchdatetime=2019-02-28 12:00");
+                var directLink = TestScenarioPart.DirectLinking(Driver, "/question/direct/PW1685MaleAdult/24/Sexual Concerns/AL74HL/?answers=2,3,2,2,2,3,3,0,0,2&dossearchdatetime=2020-02-01 12:00");
                 Assert.IsTrue(directLink.Driver.FindElement(By.Id("DosCheckCapacitySummaryResult_Success_Services_0__Id")).GetAttribute("value") == "2000005832");
             }
 
