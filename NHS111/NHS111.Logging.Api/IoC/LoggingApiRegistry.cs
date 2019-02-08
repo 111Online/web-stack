f051b3ed20f230633953dda336e97e360db5c6a8
Patrick <patrick.hughes1@nhs.net>
2019-02-08 10:42:42 +0000

    added monitor controller and ioc

---
+++ NHS111/NHS111.Logging.Api/IoC/LoggingApiRegistry.cs
@@ -0,0 +1,21 @@
+﻿using NHS111.Models.IoC;
+using NHS111.Utils.IoC;
+using StructureMap;
+using StructureMap.Graph;
+
+namespace NHS111.Logging.Api.IoC
+{
+    public class LoggingApiRegistry : Registry
+    {
+        public LoggingApiRegistry()
+        {
+            IncludeRegistry<UtilsRegistry>();
+            Scan(scan =>
+            {
+                scan.TheCallingAssembly();
+                scan.WithDefaultConventions();
+            });
+        }
+
+    }
+}
