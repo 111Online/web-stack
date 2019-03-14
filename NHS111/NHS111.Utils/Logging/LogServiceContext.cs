72920ebcdd8f4d026a9e531eff49eb6170140434
Patrick Hughes <patrick.hughes1@nhs.net>
2019-03-14 14:13:59 +0000

    reinstate logging changes

--- NHS111/NHS111.Utils/Logging/LogServiceContext.cs
+++ NHS111/NHS111.Utils/Logging/LogServiceContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
@@ -8,8 +9,9 @@ namespace NHS111.Utils.Logging
 {
     public class LogServiceContext : ILogServiceContext
     {
-        private readonly CloudTable _table;
-
+        private List<CloudTable> _tables = new List<CloudTable>();
+        private string _defaultStorageTableName;
+        private readonly CloudStorageAccount _storageAccount;
         public LogServiceContext(string accountName, string accountKey, string storageTable)
             : this(new StorageCredentials(accountName, accountKey), storageTable)
         {
@@ -17,27 +19,47 @@ namespace NHS111.Utils.Logging
 
         public LogServiceContext(StorageCredentials credentials, string storageTable)
         {
-            var account = new CloudStorageAccount(credentials, true);
-
-            var client = account.CreateCloudTableClient();
-
-            _table = client.GetTableReference(storageTable);
-            _table.CreateIfNotExists();
+            _storageAccount = new CloudStorageAccount(credentials, true);
+            _defaultStorageTableName = storageTable;
+            SetTableStorage(storageTable);
         }
 
-        public void Log<T>(T entity) where T : ITableEntity
+        private CloudTable SetTableStorage(string storageTable)
+        {
+            var client = _storageAccount.CreateCloudTableClient();
+            
+            var table = client.GetTableReference(storageTable);
+            table.CreateIfNotExists();
+            _tables.Add(table);
+            return table;
+        }
+
+        private CloudTable GetTable(string storageTable)
+        {
+            if (_tables.Exists(t => t.Name == storageTable)) return _tables.Where(t => t.Name == storageTable).First();
+            return SetTableStorage(storageTable);
+        }
+
+        public void Log<T>(T entity, string tableName) where T : ITableEntity
         {
             Action doWriteToTable = () =>
             {
                 var insertOperation = TableOperation.Insert(entity);
-                _table.Execute(insertOperation);
+                GetTable(tableName).Execute(insertOperation);
             };
             doWriteToTable.BeginInvoke(null, null);
+
+        }
+
+        public void Log<T>(T entity) where T : ITableEntity
+        {
+            Log<T>(entity, _defaultStorageTableName);
         }
     }
 
     public interface ILogServiceContext
     {
         void Log<T>(T entity) where T : ITableEntity;
+        void Log<T>(T entity, string tableName) where T : ITableEntity;
     }
 }
