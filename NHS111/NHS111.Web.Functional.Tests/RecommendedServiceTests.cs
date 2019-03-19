a88e6cf85095b38358e03b3cba927b854e5445bd
Patrick Hughes <patrick.hughes1@nhs.net>
2019-03-19 15:33:39 +0000

    added recommendedpage class and utils

---
+++ NHS111/NHS111.Web.Functional.Tests/RecommendedServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using NHS111.Web.Functional.Utils;
+using NHS111.Web.Functional.Utils.ScreenShot;
+using NUnit.Framework;
+
+namespace NHS111.Web.Functional.Tests
+{
+    [TestFixture]
+    [ScreenShotComparison]
+    public class RecommendedServiceTests : BaseTests
+    {
+        [Test]
+        public void NumsasPharmacistService()
+        {
+            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Emergency Prescription", TestScenerioSex.Male, TestScenerioAgeGroups.Adult);
+
+            questionPage.VerifyQuestion("Can you get in touch with your GP or usual pharmacy?");
+            var recommendedServicePage = questionPage
+                .Answer(2)
+                .Answer(2)
+                .Answer<OutcomePage>(1)
+                .ClickShowServices();
+
+            recommendedServicePage.VerifyCallout();
+            recommendedServicePage.VerifyServiceDetails();
+            recommendedServicePage.VerifyOtherServices();
+            recommendedServicePage.CompareAndVerify("1");
+        }
+    }
+}
