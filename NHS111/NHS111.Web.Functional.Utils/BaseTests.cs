63df9a8e049bf0e33f98c67bdfb977034f254541
Shane Hudson <shane@shanehudson.net>
2019-04-03 09:57:39 +0100

    Changing window size.

--- NHS111/NHS111.Web.Functional.Utils/BaseTests.cs
+++ NHS111/NHS111.Web.Functional.Utils/BaseTests.cs
@@ -18,21 +18,15 @@ namespace NHS111.Web.Functional.Utils
             Driver = new ChromeDriver();
             // Ideally we could have multiple size screenshots
             // for Visual Regression Test MVP this uses the same width as Andria's Selenium screenshots (1232px)
-            Driver.Manage().Window.Size = new Size(1232, 1000);
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--window-size=1232,1000");
+            Driver = new ChromeDriver(chromeOptions);
         }
 
         [TestFixtureTearDown]
         public void TearDownTestFixture()
         {
-            try
-            {
-                Driver.Quit();
-                Driver.Dispose();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
+            Driver.Quit();
         }
 
         [TearDown]
