36b9f53e1a9e785c52c1664c15e17d905e229e06
Adam Naylor <adamnaylor@gmail.com>
2019-04-15 10:47:30 +0100

    Adds fix to basic auth so it can be configured on and off

---
+++ NHS111/NHS111.Features/BasicAuthFeature.cs
@@ -0,0 +1,12 @@
+﻿
+namespace NHS111.Features {
+    using Defaults;
+
+    public interface IBasicAuthFeature : IFeature { }
+
+    public class BasicAuthFeature : BaseFeature, IBasicAuthFeature {
+        public BasicAuthFeature() {
+            DefaultIsEnabledSettingStrategy = new EnabledByDefaultSettingStrategy();
+        }
+    }
+}
