2fe5aaf39885c00362df6b20957abbc4301f002a
taxworthy <tom.axworthy@nhs.net>
2019-04-18 10:32:41 +0100

    update integration dos tests
    Add checks for contact details fields for Dos Service

--- NHS111/NHS111.Integration.DOS.Api.Functional.Tests/IntegrationDOSApiTests.cs
+++ NHS111/NHS111.Integration.DOS.Api.Functional.Tests/IntegrationDOSApiTests.cs
@@ -66,10 +66,16 @@ namespace NHS111.Integration.DOS.Api.Functional.Tests
         {
             var serviceTypeField = response.ServiceType;
             Assert.IsNotNull(serviceTypeField.Id);
+            Assert.IsNotNull(serviceTypeField.OdsCode);
+            Assert.IsNotNull(serviceTypeField.ContactDetails[0].Name);
+            Assert.IsNotNull(serviceTypeField.ContactDetails[0].Tag);
+            Assert.IsNotNull(serviceTypeField.ContactDetails[0].Order);
+            Assert.IsNotNull(serviceTypeField.ContactDetails[0].Value);
 
             Assert.IsNotNull(response.Id);
             Assert.IsNotNull(response.Capacity);
             Assert.IsNotNull(response.Name);
+            Assert.IsNotNull(response.ContactDetails);
             Assert.IsNotNull(response.ContactDetails);
             Assert.IsNotNull(response.Address);
             Assert.IsNotNull(response.PostCode);
