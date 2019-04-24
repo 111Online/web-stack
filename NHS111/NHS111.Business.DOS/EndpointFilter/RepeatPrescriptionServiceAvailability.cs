17ee1bad3466ac23ae619f72bcced0f2d8fad054
Patrick <patrick.hughes1@nhs.net>
2019-04-24 17:18:55 +0100

    added repeat prescription service filter

---
+++ NHS111/NHS111.Business.DOS/EndpointFilter/RepeatPrescriptionServiceAvailability.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHS111.Models.Models.Business;
+
+namespace NHS111.Business.DOS.EndpointFilter
+{
+    public class RepeatPrescriptionServiceAvailability : ServiceAvailability
+    {
+        private IServiceAvailabilityProfile _serviceAvailabilityProfile;
+
+        public RepeatPrescriptionServiceAvailability(IServiceAvailabilityProfile serviceAvailabilityProfile,
+            DateTime dispositionDateTime, int dispostionTimeframe)
+            : base(serviceAvailabilityProfile, dispositionDateTime, dispostionTimeframe)
+        {
+            _serviceAvailabilityProfile = serviceAvailabilityProfile;
+        }
+
+        public override List<DosService> Filter(List<DosService> resultsToFilter)
+        {
+            var itkservicestoRetain = GetSpecifiedOpenITKServices(resultsToFilter);
+            var filteredResults = resultsToFilter.Where(
+                s => !_serviceAvailabilityProfile.ServiceTypeIdBlacklist.Contains((int) s.ServiceType.Id)).ToList();
+            filteredResults.AddRange(itkservicestoRetain.Where(NotDuplicateMessage(filteredResults)));
+            return filteredResults;
+        }
+    }
+}
