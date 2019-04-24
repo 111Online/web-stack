2b91d50d8d788ed547f80298fc460eb020da3695
Adam Naylor <adamnaylor@gmail.com>
2019-04-24 13:25:19 +0100

    Refactors postcode tests and home viewmodel

--- NHS111/NHS111.Web.Presentation/Validators/PostCodeAllowedValidator.cs
+++ NHS111/NHS111.Web.Presentation/Validators/PostCodeAllowedValidator.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using NHS111.Models.Models.Web.Validators;
 using NHS111.Models.Models.Web.CCG;
 using NHS111.Web.Presentation.Builders;
 using System.Text.RegularExpressions;
 using NHS111.Features;
+
 namespace NHS111.Web.Presentation.Validators
 {
     public class PostCodeAllowedValidator : IPostCodeAllowedValidator
     {
         private readonly ICCGModelBuilder _ccgModelBuilder;
         private readonly IAllowedPostcodeFeature _allowedPostcodeFeature;
-
-        private CCGModel _ccg;
         
         public PostCodeAllowedValidator(IAllowedPostcodeFeature allowedPostcodeFeature, ICCGModelBuilder ccgModelBuilder)
         {
@@ -24,23 +19,22 @@ namespace NHS111.Web.Presentation.Validators
         }
         public PostcodeValidatorResponse IsAllowedPostcode(string postcode)
         {
-            if (string.IsNullOrWhiteSpace(postcode)) return PostcodeValidatorResponse.InvalidSyntax;
-            Regex regex = new Regex(@"^[a-zA-Z0-9]+$");
-            if(!regex.IsMatch(postcode.Replace(" ", ""))) return PostcodeValidatorResponse.InvalidSyntax;
-            Task<CCGModel> ccgModelBuildertask = Task.Run<CCGModel>(async () => await _ccgModelBuilder.FillCCGModel(postcode));
-            if (!_allowedPostcodeFeature.IsEnabled) return PostcodeValidatorResponse.InPathwaysArea;
-            _ccg = ccgModelBuildertask.Result;
-            if (_ccg.Postcode == null) return PostcodeValidatorResponse.PostcodeNotFound;
+            if (string.IsNullOrWhiteSpace(postcode))
+               return PostcodeValidatorResponse.InvalidSyntax;
+            if(!_alphanumericRegex.IsMatch(postcode.Replace(" ", "")))
+                return PostcodeValidatorResponse.InvalidSyntax;
+            var ccgModelBuilderTask = Task.Run<CCGModel>(async () => await _ccgModelBuilder.FillCCGModel(postcode));
+            if (!_allowedPostcodeFeature.IsEnabled)
+                return PostcodeValidatorResponse.InPathwaysArea;
+            CcgModel = ccgModelBuilderTask.Result;
+            if (CcgModel.Postcode == null)
+                return PostcodeValidatorResponse.PostcodeNotFound;
+
             return PostcodeValidatorResponse.InPathwaysArea;
         }
 
-        public CCGModel CcgModel
-        {
-            get { return _ccg; }
-        }
+        public CCGModel CcgModel { get; private set; }
+            
+        private readonly Regex _alphanumericRegex = new Regex(@"^[a-zA-Z0-9]+$");
     }
-
-
-   
-
 }
