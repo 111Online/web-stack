5c096b6ad0e08730c856253cd3d1eb60d4ef7f0d
Shane Hudson <shane@shanehudson.net>
2019-06-03 09:56:26 +0100

    This fixes the recomended service page map, where there was a bug that caused it to not display.

--- NHS111/NHS111.Web.Frontend/src/js/jquery-service-details.js
+++ NHS111/NHS111.Web.Frontend/src/js/jquery-service-details.js
@@ -3,18 +3,23 @@
  * entry-map.js is the file for the map itself
  */
 jQuery(function () {
+    if ($(".service-details__map").length == 0) return
 
     // This ensures the map loads properly and that JS is enabled
-    var serviceListings = $("details:not([open])").has(".service-details__map")
+    var serviceListings = $(".service-listing:not([open])").has(".service-details__map")
     if (serviceListings.length) serviceListings.one("click", loadMapForService)
 
-    function loadMapForService() {
-        var serviceID = $("[data-id]", this)[0].getAttribute('data-id')
+    var mapsOutsideOfListings = $(".service-details__map:not(.service-listing .service-details__map)")
+    mapsOutsideOfListings.toArray().forEach((val) => loadMapForService(val))
+
+    function loadMapForService(element) {
+        var el = element.currentTarget ? $('.service-details__map', this)[0] : element
+        var serviceID = el.getAttribute('data-id')
         var iframe = document.createElement('iframe')
         iframe.src = '/map/'
         iframe.setAttribute('role', 'presentation')
         iframe.className += ' service-map'
-        $('.service-details__map', this).replaceWith(iframe)
+        el.replaceWith(iframe)
 
         $(iframe).one('load', function () {
             $(iframe).show()
