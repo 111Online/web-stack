9a3cad4aa583063c60f1a197c772592c975ffd69
Adam Naylor <adamnaylor@gmail.com>
2019-06-17 09:11:43 +0100

    Adds session attribute to EP routes

--- NHS111/NHS111.Web/Controllers/PathwayController.cs
+++ NHS111/NHS111.Web/Controllers/PathwayController.cs
@@ -2,19 +2,22 @@
 namespace NHS111.Web.Controllers {
     using System.Web.Mvc;
     using Models.Models.Web;
+    using Utils.Filters;
 
     public class PathwayController
         : Controller {
 
         [Route("pathway/{pathwayNo}")]
+        [SetSessionIdFilter]
         public ActionResult Get(string pathwayNo, LocationViewModel model) {
             model.PathwayNo = pathwayNo.ToUpper();
             return View("../location/location", model);
         }
 
         [Route("emergency-prescription")]
-        public ActionResult GetEP() {
-            return Get(_emergencyPrescriptionPathwayNo, new LocationViewModel());
+        [SetSessionIdFilter]
+        public ActionResult GetEP(LocationViewModel model) {
+            return Get(_emergencyPrescriptionPathwayNo, model);
         }
 
         private string _emergencyPrescriptionPathwayNo = "PW1827";
