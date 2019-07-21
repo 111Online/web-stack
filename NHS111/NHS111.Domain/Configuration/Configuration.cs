7be1bc6f9358133222aec98127d92fe6d3aa179e
Patrick <patrick.hughes1@nhs.net>
2019-07-21 10:04:07 +0100

    updates service definition domain model

--- NHS111/NHS111.Domain/Configuration/Configuration.cs
+++ NHS111/NHS111.Domain/Configuration/Configuration.cs
@@ -8,10 +8,16 @@ namespace NHS111.Domain.Configuration
         {
             return ConfigurationManager.AppSettings["GraphDbUrl"];
         }
+
+        public string GetPathwaysVersion()
+        {
+            return ConfigurationManager.AppSettings["PathwaysVersion"];
+        }
     }
 
     public interface IConfiguration
     {
         string GetGraphDbUrl();
+        string GetPathwaysVersion();
     }
 }
