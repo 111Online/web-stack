4852f80cd8bb2f926d10e1e344f0caa1f4c28a25
Shane Hudson <shane@shanehudson.net>
2019-08-30 14:42:30 +0100

    Moving dependencies from files to npm modules
    I had an issue where by pulling from npm instead of files, it meant it would run through the entire dependency graph. As we manage it more manually by putting jQuery at the top of the page, this became an issue.
    
    So in the end I've gone with solution of code splitting where pretty much just jQuery goes into common.js with bundle-head and bundle using that. There's a small hack where cookie is now in head so that the code splitting works as expected.
    
    Note that this is due to moving licensed files away so that we can
    publicly mirror on GitHub.

--- NHS111/NHS111.Web.Frontend/src/js/entry-head.js
+++ NHS111/NHS111.Web.Frontend/src/js/entry-head.js
@@ -2,8 +2,17 @@
  * Due to inline scripts throughout the site, this file creates a bundle
  * for any dependencies (such as jquery) that are absolutely required at the beginning
  * of page load. The rest go at the end, for performance.
- */
+ *
+ * [1]: jQuery cookie would ideally be imported in entry-main to be served at the bottom of the site
+ *      however as part of swapping from importing from file to from npm module,
+ *      the modules now import all dependencies in each file. So jQuery was being imported twice.
+ *      The fix for now is to use webpack.config.js code splitting in such a way that
+ *      it creates the common.js file for jQuery and other shared dependencies.
+ *      However this was not working as just requiring jQuery in this file did not count as a dependency.
+ *      So we import one jQuery plugin (cookie) so that the dependency graph is correct for the code splitting.
+ **/
 
-import './vendor/details.min.js'
 global.$ = global.jQuery = require('jquery')
-global.Spinner = require('./vendor/spin.js')
+global.Spinner = require('spin.js')
+import './vendor/details.min.js'
+import 'jquery.cookie' // [1]
