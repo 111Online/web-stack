4e014337cf8bc97261f7d52e2af1b2955695a071
Shane Hudson <shane@shanehudson.net>
2019-09-16 14:07:16 +0100

    Removing unused bits

--- NHS111/NHS111.Web.Frontend/gulpfile.js
+++ NHS111/NHS111.Web.Frontend/gulpfile.js
@@ -1,13 +1,10 @@
-const path = require('path'),
-    gulp = require('gulp'),
+const gulp = require('gulp'),
     runSequence = require('run-sequence'),
     del = require('del'),
     sass = require('gulp-sass'),
-    flatten = require('gulp-flatten'),
     importOnce = require('node-sass-import-once'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
-    inject = require('gulp-inject'),
     cssnano = require('cssnano'),
     mocha = require('gulp-mocha'),
     fs = require('fs'),
@@ -18,8 +15,7 @@ const paths = {
   srcScripts: `${__dirname}/src/scripts`,
   srcScss: `${__dirname}/src/styles`,
   srcImages: `${__dirname}/src/images`,
-  dist: `${__dirname}/../NHS111.Web/Content/`,
-  distAssets: `${__dirname}/../NHS111.Web/Content/`
+  dist: `${__dirname}/../NHS111.Web/Content/`
 }
 
 gulp.task('build-if-missing', () => {
@@ -42,24 +38,9 @@ gulp.task('clean:dist', () => {
     return del([`${paths.dist}/`], { force: true })
 })
 
-//gulp.task('copy:styles:dist', () => {
-//  return gulp.src([
-//    `${paths.srcScss}/**/*.scss`,
-//    `!${paths.srcScss}/components/**/*.scss`
-//  ])
-//  .pipe(gulp.dest(paths.distScss))
-//})
-
-//gulp.task('copy:styles:components:dist', () => {
-//  return gulp.src(`${paths.srcScss}/components/**/*.scss`)
-//    .pipe(flatten())
-//    .pipe(gulp.dest(`${paths.distScss}/components`))
-//})
-
-// Copy images
 gulp.task('copy:images', () => {
   return gulp.src(`${paths.srcImages}/**/*`)
-    .pipe(gulp.dest(`${paths.distAssets}/images`))
+    .pipe(gulp.dest(`${paths.dist}/images`))
 })
 
 gulp.task('lint:styles', () => {
@@ -95,20 +76,6 @@ gulp.task('test:scripts', function() {
     })
 })
 
-//gulp.task('inject:styles:dist', () => {
-//  return gulp.src(`${paths.distScss}/components/_index.scss`)
-//    .pipe(inject(gulp.src([
-//      `${paths.distScss}/components/*.scss`,
-//      `!${paths.distScss}/components/_index.scss`
-//    ]), {
-//      starttag: '//inject:{{ext}}',
-//      endtag: '//endinject',
-//      relative: true,
-//      transform: (filePath) => `@import "${filePath}";`
-//    }))
-//    .pipe(gulp.dest(`${paths.distScss}/components`))
-//})
-
 gulp.task('compile:styles:dist', () => {
   return gulp.src(`${paths.srcScss}/*.scss`)
     .pipe(sass({
@@ -121,23 +88,18 @@ gulp.task('compile:styles:dist', () => {
       sass.logError.call(this, err)
       process.exit(1)
     }))
-    .pipe(postcss([autoprefixer({ grid: false }), cssnano()]))
-    .pipe(gulp.dest(`${paths.distAssets}/css`))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest(`${paths.dist}/css`))
 })
 
 
 gulp.task('compile:scripts', () => {
     return gulp.src([])
                .pipe(pipedWebpack(require('./webpack.config.js')))
-               .pipe(gulp.dest(`${paths.distAssets}/js`))
+               .pipe(gulp.dest(`${paths.dist}/js`))
 });
 
 gulp.task('build', cb => {
-  //runSequence(
-  //  ['copy:styles:dist', 'copy:styles:components:dist', 'copy:images'],
-  //    'inject:styles:dist', 'compile:styles:dist', 'compile:scripts',
-  //  cb
-  //)
   runSequence(
     'copy:images', 'compile:styles:dist', 'compile:scripts',
     cb
