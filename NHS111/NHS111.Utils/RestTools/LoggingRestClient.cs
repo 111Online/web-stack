672461e3b15f182596c5c03242a45a232b3f127d
james.tiffen@nhs.net <james.tiffen@nhs.net>
2019-10-15 14:58:39 +0100

    Set restcleint to support full urls as well as relative urls

--- NHS111/NHS111.Utils/RestTools/LoggingRestClient.cs
+++ NHS111/NHS111.Utils/RestTools/LoggingRestClient.cs
@@ -9,11 +9,13 @@ namespace NHS111.Utils.RestTools
     public class LoggingRestClient : RestClient
     {
         private readonly ILog _logger;
+        private string _baseUrlDefault;
         public LoggingRestClient(string baseUrl, ILog logger) : base(baseUrl)
         {
             ServicePointManager.DefaultConnectionLimit = 5;
             _logger = logger;
             InitializeSerialisationHandlers();
+            _baseUrlDefault = baseUrl;
         }
 
         private void InitializeSerialisationHandlers()
@@ -27,11 +29,24 @@ namespace NHS111.Utils.RestTools
 
         public override async Task<IRestResponse<T>> ExecuteTaskAsync<T>(IRestRequest request)
         {
+            base.BaseUrl = new Uri(_baseUrlDefault);
+            if (RequestContainsAuthority(request))
+            {
+                base.BaseUrl = new Uri(new Uri(request.Resource.TrimStart('/')).GetLeftPart(UriPartial.Authority));
+                request.Resource = new Uri(request.Resource.TrimStart('/')).PathAndQuery;
+            }
+            
             _logger.Info(string.Format("Request to: {0}{1} performed", BaseUrl,request.Resource));
+
             var response = await base.ExecuteTaskAsync<T>(request);
             if(response != null && response.StatusCode !=  HttpStatusCode.OK) _logger.Error(String.Format("Request to: {0}{1} returned with Error Code: {2} and response: {3}", BaseUrl, request.Resource, response.StatusCode, response.ErrorMessage));
             else _logger.Error(String.Format("Request to: {0}{1} failed", BaseUrl, request.Resource));
             return response;
+        }
+
+        private bool RequestContainsAuthority(IRestRequest request)
+        {
+            return Uri.IsWellFormedUriString(request.Resource.TrimStart('/'), UriKind.Absolute);
         }
     }
 }
