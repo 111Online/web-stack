4f66e77a91ac1b1b7b6d71e03e435171df83562b
Shane Hudson <shane@shanehudson.net>
2019-11-27 13:04:07 +0000

    Add SetSessionId to help pages so if those are the first pages, it is logged correctly

--- NHS111/NHS111.Web/Controllers/HelpController.cs
+++ NHS111/NHS111.Web/Controllers/HelpController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.Mvc;
 using NHS111.Models.Models.Domain;
 using NHS111.Utils.Attributes;
+using NHS111.Utils.Filters;
 using NHS111.Web.Presentation.Configuration;
 using RestSharp;
 
@@ -9,21 +10,25 @@ namespace NHS111.Web.Controllers
     [LogHandleErrorForMVC]
     public class HelpController : Controller
     {
+        [SetSessionIdFilter]
         public ActionResult Cookies()
         {
             return View();
         }
-
+        
+        [SetSessionIdFilter]
         public ActionResult Privacy()
         {
             return View();
         }
-
+        
+        [SetSessionIdFilter]
         public ActionResult Terms()
         {
             return View();
         }
-
+        
+        [SetSessionIdFilter]
         public ActionResult Browsers()
         {
             return View();
