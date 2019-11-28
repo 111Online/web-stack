34ce425b759ad70f8afe76f83b39fa9ed7f1268a
Shane Hudson <shane@shanehudson.net>
2019-11-28 16:02:17 +0000

    Remove wrong comment

--- NHS111/NHS111.Models/Models/Web/Enums/EventType.cs
+++ NHS111/NHS111.Models/Models/Web/Enums/EventType.cs
@@ -3,7 +3,6 @@ using Newtonsoft.Json.Converters;
 
 namespace NHS111.Models.Models.Web.Enums
 {
-    // If this enum is updated, the Logging API needs the same change made to it
 
     [JsonConverter(typeof(StringEnumConverter))]
     public enum EventType
