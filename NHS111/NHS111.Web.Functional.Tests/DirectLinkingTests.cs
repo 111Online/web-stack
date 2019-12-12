359d89968b6bcc36040de633d23f76200e937bc4
AndriaTurner <Andria.Turner@nhs.net>
2019-12-12 19:54:03 +0000

    Post UAT Does refresh:
    Tweaked a couple of postcodes
    commented out 'OtherServicesNotShown()' until we decide on a way forward for that test.
    removed '&dos=uat' as no longer working, but isnt needed on this test now
    Added a time to thest 'NoRecommendedServicesPage()' to ensure its a time no other services return

--- NHS111/NHS111.Web.Functional.Tests/DirectLinkingTests.cs
+++ NHS111/NHS111.Web.Functional.Tests/DirectLinkingTests.cs
@@ -43,7 +43,7 @@ namespace NHS111.Web.Functional.Tests
             public void Correct999Service()
             {
                 // 999 Validation page shows with correct service
-                var directLink = TestScenarioPart.DirectLinking(Driver, "/question/direct/PW755MaleAdult/20/Headache/AL7 4HL/?answers=0,0,0,2,0,0&dos=uat");
+                var directLink = TestScenarioPart.DirectLinking(Driver, "/question/direct/PW755MaleAdult/20/Headache/AL7 4HL/?answers=0,0,0,2,0,0");
                 directLink.VerifyBookACall("2000011053");
             }
 
@@ -82,10 +82,10 @@ namespace NHS111.Web.Functional.Tests
                 directLink.VerifyOutOfArea();
             }
 
-            [Test]
+            [Test] //added time on to find a time when nothing returns, Pharmacies nationwide have had digital referral role added and too many to remove.
             public void NoRecommendedServicesPage()
             {
-                var directLink = TestScenarioPart.DirectLinking(Driver, "/question/direct/PW1827MaleAdult/33/Emergency%20Prescription%20111%20online/LS175DT/?answers=0,1,0");
+                var directLink = TestScenarioPart.DirectLinking(Driver, "/question/direct/PW1827MaleAdult/33/Emergency%20Prescription%20111%20online/LS175DT/?answers=0,1,0&dossearchdatetime=2020-02-01 23:30");
                 directLink.VerifyNoRecommendedServices();
             }
 
