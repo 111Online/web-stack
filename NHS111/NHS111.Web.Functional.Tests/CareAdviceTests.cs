537d06906d4e7257a062022abd64e0167d548343
AndriaTurner <Andria.Turner@nhs.net>
2019-12-24 12:31:37 +0000

    Updates so far:
    Updated a few reworded questions after checking in PAT
    Found alternative scenario for excluded care advice
    Found alternative route for age logic test as part of the journey has changed

--- NHS111/NHS111.Web.Functional.Tests/CareAdviceTests.cs
+++ NHS111/NHS111.Web.Functional.Tests/CareAdviceTests.cs
@@ -9,17 +9,27 @@ namespace NHS111.Web.Functional.Tests
         //Ensures that Medication, pain and/or fever care advice is not additionally included in results
         public void CareAdvice_Is_Correctly_Excluded()
         {
-            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Vaginal Swellings", TestScenerioSex.Female, 20);
+            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Headache", TestScenerioSex.Female, 20);
 
             var outcomePage = questionPage
-                .Answer(1)
+                .Answer(3)
+                .Answer(3)
+                .Answer(4)
+                .Answer(3)
+                .Answer(3)
+                .Answer(5)
+                .Answer(3)
+                .Answer(3)
+                .Answer(3)
                 .Answer(3)
                 .Answer(3)
                 .Answer(3)
                 .Answer(1)
-                .Answer<OutcomePage>("Yes");
+                .Answer(3)
+                .Answer(4)
+                .Answer<OutcomePage>("I've had it for 3 days or more");
 
-            outcomePage.VerifyCareAdvice(new [] { "Abdominal pain", "Medication, pain and/or fever, abdominal/urinary", "Fever" });
+            outcomePage.VerifyCareAdvice(new [] { "Medication, pain and/or fever", "Fever" });
         }
     }
 }
