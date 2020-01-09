eb709c055d5dc4bb9cec2f2d713a51f95464ed2c
AndriaTurner <Andria.Turner@nhs.net>
2020-01-09 16:42:41 +0000

    Added in a Test for CAT1
    while this confirms the new version 18 route is available, the test does not differentiate between the outcome groups Cat1 and Cat2.  Do we need to add in anything else?

--- NHS111/NHS111.Web.Functional.Tests/EndpointJourneyTests.cs
+++ NHS111/NHS111.Web.Functional.Tests/EndpointJourneyTests.cs
@@ -48,6 +48,22 @@ namespace NHS111.Web.Functional.Tests
         }
 
         [Test]
+        public void Call999EndpointJourneyCAT1()
+        {
+            var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Skin Problems", TestScenerioSex.Male, TestScenerioAgeGroups.Adult);
+
+            questionPage.VerifyQuestion("What is the main problem?");
+            var outcomePage = questionPage
+                .Answer(1)
+                .Answer(3)
+                .AnswerSuccessiveByOrder(1, 2)
+                .Answer<OutcomePage>(1);
+
+            outcomePage.VerifyOutcome("Phone 999 now for an ambulance");
+            outcomePage.CompareAndVerify(outcomePage, "1");
+        }
+
+        [Test]
         public void PharmacyEndpointJourney()
         {
             var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Eye or Eyelid Problems", TestScenerioSex.Male, TestScenerioAgeGroups.Adult);
