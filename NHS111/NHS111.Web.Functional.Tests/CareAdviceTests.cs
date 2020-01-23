84178d46cf334869f1f5c007f955fe7e5700ddb4
AndriaTurner <Andria.Turner@nhs.net>
2020-01-23 12:08:22 +0000

    Updated regression test to show Stem removal introduced in V17 is no longer in place.

--- NHS111/NHS111.Web.Functional.Tests/CareAdviceTests.cs
+++ NHS111/NHS111.Web.Functional.Tests/CareAdviceTests.cs
@@ -32,7 +32,7 @@ namespace NHS111.Web.Functional.Tests
             outcomePage.VerifyCareAdvice(new [] { "Medication, pain and/or fever", "Fever" });
         }
         [Test]
-        //Ensures that Medication, pain and/or fever care advice is not additionally included in results
+        //Ensures that Care advice stem removal is no longer in place. CX221005. Text no longer appearing needs to have individual asertions.
         public void CareAdvice_stem_is_no_longer_removed()
         {
             var questionPage = TestScenerios.LaunchTriageScenerio(Driver, "Vaginal Discharge", TestScenerioSex.Female, 30);
@@ -46,6 +46,18 @@ namespace NHS111.Web.Functional.Tests
                 .Answer<OutcomePage>("No");
 
             outcomePage.VerifyCareAdvice(new[] { "Genital discharge/irritation", "Medication, pain and/or fever", "Fever" });
+            var sexualhealthAdviceExists = outcomePage.CareAdviceExists(new[] { "If you are sexually activ:", "Don't have sex until the problem is sorted out." });
+            var sexualhealth1AdviceDoesNotExists = outcomePage.CareAdviceExists(new[] { "Don't have sex until the problem is sorted out.", "Wear loose-fitting cotton clothes and underwear, as they allow the air to circulate.", "If you need to be seen by a GP or out of hours service, take a wee sample in a clean jar.", "If you're sexually active:", "Don't have sex until the problem has cleared up." });
+            var sexualhealth2AdviceDoesNotExists = outcomePage.CareAdviceExists(new[] { "Wear loose-fitting cotton clothes and underwear, as they allow the air to circulate.", "If you need to be seen by a GP or out of hours service, take a wee sample in a clean jar.", "If you're sexually active:", "Don't have sex until the problem has cleared up." });
+            var sexualhealth3AdviceDoesNotExists = outcomePage.CareAdviceExists(new[] { "If you need to be seen by a GP or out of hours service, take a wee sample in a clean jar.", "If you're sexually active:", "Don't have sex until the problem has cleared up." });
+            var sexualhealth4AdviceDoesNotExists = outcomePage.CareAdviceExists(new[] { "If you're sexually active:", "Don't have sex until the problem has cleared up." });
+       
+            Assert.IsTrue(sexualhealthAdviceExists);
+            Assert.IsFalse(sexualhealth1AdviceDoesNotExists);
+            Assert.IsFalse(sexualhealth2AdviceDoesNotExists);
+            Assert.IsFalse(sexualhealth3AdviceDoesNotExists);
+            Assert.IsFalse(sexualhealth4AdviceDoesNotExists);
+
         }
     }
 }
