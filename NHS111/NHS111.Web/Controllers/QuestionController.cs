4a4ccda3751fb1e97e80daa5620572fd9e8564de
james.tiffen@nhs.net <james.tiffen@nhs.net>
2020-03-24 17:17:10 +0000

    Added validation for text questiontypes and seprate propety to pass user input fields

--- NHS111/NHS111.Web/Controllers/QuestionController.cs
+++ NHS111/NHS111.Web/Controllers/QuestionController.cs
@@ -103,7 +103,7 @@ namespace NHS111.Web.Controllers {
         {
           
 
-            if (!ModelState.IsValidField("SelectedAnswer")) return View("Question", model);
+            if (!ModelState.IsValidField("SelectedAnswer") || !ModelState.IsValidField("AnswerInputValue")) return View("Question", model);
 
             
             if (model.NodeType == NodeType.Page && model.Content!=null && model.Content.StartsWith("!CustomView!"))
@@ -111,6 +111,7 @@ namespace NHS111.Web.Controllers {
             ModelState.Clear();
             var nextModel = await GetNextJourneyViewModel(model);
             var viewRouter = _viewRouter.Build(nextModel, ControllerContext);
+          //  model.SelectedAnswer = String.Empty; // reset answer
             return View(viewRouter.ViewName, nextModel);
         }
 
@@ -397,6 +398,7 @@ namespace NHS111.Web.Controllers {
         }
 
         private async Task<QuestionWithAnswers> GetNextNode(QuestionViewModel model) {
+
             var answer = JsonConvert.DeserializeObject<Answer>(model.SelectedAnswer);
             var serialisedState = HttpUtility.UrlEncode(model.StateJson);
             var request = new JsonRestRequest(_configuration.GetBusinessApiNextNodeUrl(model.PathwayId, model.NodeType, model.Id, serialisedState, true), Method.POST);
