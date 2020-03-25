99e34bcd1389bfc1c9049d18d4617f60c048e4d0
james.tiffen@nhs.net <james.tiffen@nhs.net>
2020-03-25 12:32:55 +0000

    Added support for telephonNumber type

--- NHS111/NHS111.Models/Models/Web/Validators/QuestionViewModelValidator.cs
+++ NHS111/NHS111.Models/Models/Web/Validators/QuestionViewModelValidator.cs
@@ -14,6 +14,12 @@ namespace NHS111.Models.Models.Web.Validators
         {
             RuleFor(q => q.SelectedAnswer).NotEmpty().When(q => q.QuestionType == QuestionType.Choice).WithMessage("Please select an answer");
             RuleFor(q => q.AnswerInputValue).NotEmpty().When(q => q.QuestionType == QuestionType.String || q.QuestionType == QuestionType.Text).WithMessage("Please select an answer");
+            RuleFor(q => q.AnswerInputValue).Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty()
+                .Must(s => s.ToCharArray().All(char.IsDigit))
+                .Must(s => s.StartsWith("0") || s.StartsWith("1"))
+                .Matches("^[0-1,\\s, +]{1}[0-9,\\s]{8,20}$")
+                .Length(9, 21).When(q => q.QuestionType == QuestionType.Telephone).WithMessage("Please give a valid uk telephone number");
         }
     }
 }
