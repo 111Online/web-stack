954dc82b603da11ab769d9a60f841506c252dd5a
Patrick Hughes <patrick.hughes1@nhs.net>
2020-03-25 15:47:19 +0000

    adds views and models for sms outcome

---
+++ NHS111/NHS111.Models/Models/Web/SendSMSOutcomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHS111.Models.Models.Web
+{
+    public class SendSmsOutcomeViewModel : OutcomeViewModel
+    {
+        public string MobileNumber { get; set; }
+        public DateTime SymptomsStartedDate { get; set; }
+        public bool LivesAlone { get; set; }
+
+    }
+}
