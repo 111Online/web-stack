5e2e23cbcc38212eab3fd7d9054929803ae8cc0e
Patrick Hughes <patrick.hughes1@nhs.net>
2020-03-25 16:49:12 +0000

    fix test

--- NHS111/NHS111.Models/Mappers/WebMappings/JourneyViewModelMapper.cs
+++ NHS111/NHS111.Models/Mappers/WebMappings/JourneyViewModelMapper.cs
@@ -54,6 +54,22 @@ namespace NHS111.Models.Mappers.WebMappings
                 .ForMember(s => s.HasResults, o => o.Ignore());
 
             Mapper.CreateMap<JourneyViewModel, SendSmsOutcomeViewModel>()
+                .ForMember(s => s.SelectedServiceId, o => o.Ignore())
+                .ForMember(s => s.DosCheckCapacitySummaryResult, o => o.Ignore())
+                .ForMember(s => s.SurgeryViewModel, o => o.Ignore())
+                .ForMember(s => s.CareAdviceMarkers, o => o.Ignore())
+                .ForMember(s => s.CareAdvices, o => o.Ignore())
+                .ForMember(s => s.SymptomGroup, o => o.Ignore())
+                .ForMember(s => s.Urgency, o => o.Ignore())
+                .ForMember(s => s.WorseningCareAdvice, o => o.Ignore())
+                .ForMember(s => s.SymptomDiscriminator, o => o.Ignore())
+                .ForMember(s => s.CurrentView, o => o.Ignore())
+                .ForMember(s => s.SurveyLink, o => o.Ignore())
+                .ForMember(s => s.Informant, opt => opt.Ignore())
+                .ForMember(s => s.UnavailableSelectedService, o => o.Ignore())
+                .ForMember(s => s.GroupedDosServices, o => o.Ignore())
+                .ForMember(s => s.HasAcceptedCallbackOffer, o => o.Ignore())
+                .ForMember(s => s.RecommendedService, opt => opt.Ignore())
                 .ForMember(s => s.MobileNumber, o => o.Ignore())
                 .ForMember(s => s.SymptomsStartedDate, o => o.Ignore())
                 .ForMember(s => s.LivesAlone, o => o.Ignore());
