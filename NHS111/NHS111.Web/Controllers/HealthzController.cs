b6c56d4a87791f4ace33fba282f1b07bd77f3f19
Sebastian <11445087+sebader@users.noreply.github.com>
2020-04-06 18:34:18 +0200

    added basic health probe controller which is open without any basic auth

---
+++ NHS111/NHS111.Web/Controllers/HealthzController.cs
@@ -0,0 +1,20 @@
+﻿using NHS111.Web.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace NHS111.Web.Controllers
+{
+    public class HealthzController : ApiController
+    {
+        [AllowAnonymousAccess]
+        public string Get()
+        {
+            return "healthy";
+        }
+    }
+}
