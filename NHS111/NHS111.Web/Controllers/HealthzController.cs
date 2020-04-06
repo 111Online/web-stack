eae749f1233e3276e837b699b2dc562620fb5d2c
Sebastian <11445087+sebader@users.noreply.github.com>
2020-04-06 21:02:22 +0200

    allowing HEAD

--- NHS111/NHS111.Web/Controllers/HealthzController.cs
+++ NHS111/NHS111.Web/Controllers/HealthzController.cs
@@ -5,13 +5,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace NHS111.Web.Controllers
 {
     public class HealthzController : ApiController
     {
+        /// <summary>
+        /// This health endpoint is used by FrontDoor
+        /// </summary>
+        /// <returns></returns>
         [AllowAnonymousAccess]
+        [HttpGet]
+        [HttpHead]
         public string Get()
         {
             return "healthy";
