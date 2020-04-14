759da5f505c8b5e20ba88e52bdef0c7e4815d62d
james.tiffen@nhs.net <james.tiffen@nhs.net>
2020-04-14 15:37:52 +0100

    Merged from master

---
+++ NHS111/NHS111.Models/Models/Business/Caching/ICachekey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHS111.Models.Models.Business.Caching
+{ 
+    public interface ICacheKey<TItem>
+    {
+        string CacheKey { get; }
+    }
+}
