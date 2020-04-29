f14e617138f76f540df990610d4d0537a3cbe9c7
Joel Walker <joel.walker1@nhs.net>
2020-04-29 17:40:42 +0100

    [ON-1958] WIP Add state for book a call with pharmacist

--- NHS111/NHS111.Models/Models/Web/IKTPrimaryCareOutComeViewModelExtenion.cs
+++ NHS111/NHS111.Models/Models/Web/IKTPrimaryCareOutComeViewModelExtenion.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,35 +12,40 @@ namespace NHS111.Models.Models.Web
 
         public static string GetHeaderText(OutcomeViewModel outcomeViewModel)
         {
-            switch (outcomeViewModel.Id)
+            var dispositionsWithCustomHeading = new Dictionary<string, string>()
             {
-                case "Dx09":
-                    return "Book a GP appointment if you don't feel better in a few days";
-                case "Dx10":
-                    return "Book a non-urgent GP appointment";
-                case "Dx16":
-                    return "Contact your GP if you don't feel better in a few days";
-                case "Dx75":
-                    return "Contact your GP within the next few days";
-                default:
-                    return string.Format("{0} {1}", outcomeViewModel.OutcomeGroup.Text, outcomeViewModel.DispositionUrgencyText);
-            }
+                { "Dx09" ,"Book a GP appointment if you don't feel better in a few days"},
+                { "Dx10" ,"Book a non-urgent GP appointment"},
+                { "Dx16" ,"Contact your GP if you don't feel better in a few days"},
+                { "Dx75" ,"Contact your GP within the next few days"},
+            };
+           
+            return dispositionsWithCustomHeading.ContainsKey(outcomeViewModel.Id) ?
+                dispositionsWithCustomHeading[outcomeViewModel.Id] 
+                : 
+                string.Format("{0} {1}", outcomeViewModel.OutcomeGroup.Text, outcomeViewModel.DispositionUrgencyText);
         }
 
-        public static string GetCannotGetAppointmentText(OutcomeViewModel outcomeViewModel)
+
+        public static bool HasPharmacistReferral(string dispositionId)
         {
-            switch (outcomeViewModel.Id)
+            var dispositionIdsWithPharmacistReferral = new List<string>()
             {
-                case "Dx08":
-                case "Dx09":
-                case "Dx10":
-                case "Dx15":
-                case "Dx16":
-                case "Dx75":
-                    return "I can't get an appointment today or tomorrow";
-                default:
-                    return "I can't get an appointment today";
-            }
+                "Dx09", "Dx10", "Dx16", "Dx75"
+            };
+            return dispositionIdsWithPharmacistReferral.Contains(dispositionId);
+        }
+
+        public static string GetCannotGetAppointmentText(string dispositionId)
+        {
+            var dispositionWithCustomCannotGetAppointmentText = new Dictionary<string, string>()
+            {
+                { "Dx08" ,"I can't get an appointment today or tomorrow"},
+                { "Dx16" ,"I can't get an appointment today or tomorrow"},
+            };
+            return dispositionWithCustomCannotGetAppointmentText.ContainsKey(dispositionId)
+                ? dispositionWithCustomCannotGetAppointmentText[dispositionId]
+                : "I can't get an appointment today";
         }
     }
 }
