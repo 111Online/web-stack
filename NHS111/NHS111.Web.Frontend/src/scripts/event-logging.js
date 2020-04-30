7acbb606f338b0c079257db5b4e60a6f64860de4
Shane Hudson <shane.hudson1@nhs.net>
2020-04-30 12:23:46 +0100

    # This is a combination of 10 commits.tree 084d4e6b90f183a94ae5a4124ae7ab62a27ec71e parent c8c309b19852c670a5e5f6fa79b825114e9157f0 author Shane Hudson <shane.hudson1@nhs.net> 1588245826 +0100 committer Shane Hudson <shane.hudson1@nhs.net> 1588771953 +0100
    # This is a combination of 8 commits.
    # This is the 1st commit message:
    
    This fixes a race condition with events on links
    
    - Fixes a typo on emergency banner and search which caused those links to not be
      tracked
    - Adds missing event to NHS.UK link on homepage
    - Updated JavaScript event-logging file to have separate events for
      links and non-links. This allows us to stop the default behaviour for
      tracked links and instead do the event tracking first followed by the
      redirect to the href provided. This redirect happens regardless of if
      the event succeeds or fails to ensure it does work as expected.
    
    # This is the commit message #2:
    
    Update timeout to 200 to see if it makes a difference on edge cases
    
    # This is the commit message #3:
    
    This removes the override of link so it can use default behaviour after event is logged
    
    # This is the commit message #4:
    
    Fixing event tracking for elements with nested elements
    
    # This is the commit message #5:
    
    Change currentTarget to this
    
    # This is the commit message #6:
    
    Don't hijack anchor links that open in a new window so logging takes place
    
    # This is the commit message #7:
    
    Split the click event handler into 2 so preventDefault works consistently
    
    # This is the commit message #8:
    
    Tidy up spacing
    
    # This is the commit message #10:
    
    Update timeout to 200 to see if it makes a difference on edge cases

--- NHS111/NHS111.Web.Frontend/src/scripts/event-logging.js
+++ NHS111/NHS111.Web.Frontend/src/scripts/event-logging.js
@@ -3,29 +3,58 @@ global.$ = global.jQuery = require('jquery')
 window.EventTypes = { Clicked: "Clicked", Error: "Error" }
 
 function initEventHandlers() {
-    $(document).on("click", "[data-event-trigger='click']", function () {
-       window.logEvent(window.EventTypes.Clicked, $(this).attr("data-event-value"))
-    })
+
+  // For all links we need to insert the click event before the new page begins loading
+  // If we don't do this, there will be a race condition where the event silently gets
+  // aborted and does not show in the dev tools network panel at all.
+
+  // Add click event handler on links that don't want to open in a new window. This hijacks
+  // the click event, do the logging and then continue with the click by redirecting
+  $(document).on("click", "[data-event-trigger='click']a:not([target=_blank])", function (e) {
+    e.preventDefault()
+    const target = $(this)
+    const value = target.attr("data-event-value")
+    const href = target.prop("href")
+
+    if (href) {
+      window.logEvent(window.EventTypes.Clicked, value).always(() => {
+        window.location = href
+      })
+    }
+  })
+
+  // Add click event handler on anchors which open in new windows and other non-link elements
+  //(such as reveals and buttons) and log normally. Links wanting to open in new window should log
+  // successfully as original window is still open
+  $(document).on("click", "a[target=_blank][data-event-trigger='click'], [data-event-trigger='click']:not(a)", function () {
+
+    window.logEvent(window.EventTypes.Clicked, $(this).attr("data-event-value"))
+
+  })
 }
 
-window.createAuditEntry = function(eventKey, eventValue) {
-    return {
-      "sessionId": $.cookie("nhs111-session-id"),
-      "journeyId": $("#journeyId").value,
-      "eventKey": eventKey,
-      "eventValue": eventValue,
-      "page": location.pathname
+window.createAuditEntry = function (eventKey, eventValue) {
+  return {
+    "sessionId": $.cookie("nhs111-session-id"),
+    "journeyId": $("#journeyId").value,
+    "eventKey": eventKey,
+    "eventValue": eventValue,
+    "page": location.pathname
   }
 }
 
-window.logEventEntry = function(auditEntry) {
-    $.post("/Auditing/Log", auditEntry)
+window.logEventEntry = function (auditEntry) {
+  return $.ajax({
+    method: "POST",
+    url: "/Auditing/Log",
+    data: auditEntry,
+    timeout: 200 // Short timeout, to ensure user is not slowed down
+  })
 }
 
 window.logEvent = function (key, value) {
   var auditEntry = window.createAuditEntry(key, value)
-  window.logEventEntry(auditEntry)
+  return window.logEventEntry(auditEntry)
 }
-
 
 initEventHandlers()
