4a74b6093d90ec41ee3eaf57df69a49314a9158e
james.tiffen@nhs.net <james.tiffen@nhs.net>
2020-05-06 08:57:44 +0100

    split out models and associated validation. Added mapping

---
+++ NHS111/NHS111.Models/Mappers/WebMappings/FromTelephoneNumberViewModelToPersonalDetailsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using NHS111.Models.Models.Web;
+using NHS111.Models.Models.Web.PersonalDetails;
+
+namespace NHS111.Models.Mappers.WebMappings
+{
+    public class FromTelephoneNumberViewModelToPersonalDetailsViewModel : Profile
+    {
+        protected override void Configure()
+        {
+            Mapper.CreateMap<TelephoneNumberViewModel, PersonalDetailViewModel>().ConvertUsing((ps, psd) =>
+            {
+                var telephoneNumberViewModel = ps.SourceValue as TelephoneNumberViewModel;
+                var personalDetails = telephoneNumberViewModel.PersonalDetailsViewModel;
+                personalDetails.UserInfo.TelephoneNumber = telephoneNumberViewModel.TelephoneNumber;
+                return personalDetails;
+            });
+        }
+    }
+}
