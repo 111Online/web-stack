da1c4082806006db9a000a16cf7e926e535cc9c9
james.tiffen@nhs.net <james.tiffen@nhs.net>
2020-05-06 08:57:44 +0100

    split out models and associated validation. Added mapping

---
+++ NHS111/NHS111.Models/Models/Web/PersonalDetails/TelephoneNumberViewModel.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Attributes;
+using NHS111.Models.Models.Web.Validators;
+
+namespace NHS111.Models.Models.Web.PersonalDetails
+{
+    [Validator(typeof(TelephoneNumberValidator))]
+    public class TelephoneNumberViewModel
+    {
+        public TelephoneNumberViewModel() { }
+        public TelephoneNumberViewModel(PersonalDetailViewModel personalDetailViewModel)
+        {
+            PersonalDetailsViewModel = personalDetailViewModel;
+        }
+        public string TelephoneNumber { get; set; }
+        public PersonalDetailViewModel PersonalDetailsViewModel { get; private set; }
+    }
+}
