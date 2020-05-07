02a90711a1db37aba3c97630000cc918beaa6910
james.tiffen@nhs.net <james.tiffen@nhs.net>
2020-05-07 08:50:34 +0100

    Removed hack to pass 2 models and used htmlHelper extension to set field ids correctly

---
+++ NHS111/NHS111.Utils/Extensions/HtmlHelperExtenstions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace NHS111.Utils.Extensions
+{
+    public static class HtmlHelperExtenstions
+    {
+        public static void RenderPartialWithPrefix(this HtmlHelper helper, string partialViewName, object model, string prefix)
+        {
+            helper.RenderPartial(partialViewName,
+                model,
+                new ViewDataDictionary { TemplateInfo = new System.Web.Mvc.TemplateInfo { HtmlFieldPrefix = prefix } });
+        }
+    }
+}
--- NHS111/NHS111.Web/Controllers/PersonalDetailsController.cs
+++ NHS111/NHS111.Web/Controllers/PersonalDetailsController.cs
@@ -147,9 +147,8 @@ namespace NHS111.Web.Controllers
 
         [HttpPost]
        
-        public async Task<ActionResult> CurrentAddress(TelephoneNumberViewModel model,PersonalDetailViewModel personalDetailViewModel)
+        public async Task<ActionResult> CurrentAddress(TelephoneNumberViewModel model)
         {
-            model.PersonalDetailsViewModel = personalDetailViewModel; // fix for this could include custom binder or TelephoneNumberViewModel inherits from PersonalDetailViewModel
             if (!ModelState.IsValid)
             {
                 return View("~\\Views\\PersonalDetails\\PersonalDetails.cshtml", model.PersonalDetailsViewModel);
