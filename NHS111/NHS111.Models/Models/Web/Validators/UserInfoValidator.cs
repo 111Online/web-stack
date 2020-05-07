d7e84a25bae421f61138d53bd3ecd1822426b413
Damian Brodrick <DABR18@hscic.gov.uk>
2020-05-07 15:43:26 +0100

    Work in Progress

--- NHS111/NHS111.Models/Models/Web/Validators/UserInfoValidator.cs
+++ NHS111/NHS111.Models/Models/Web/Validators/UserInfoValidator.cs
@@ -10,10 +10,10 @@ namespace NHS111.Models.Models.Web.Validators
     {
         public UserInfoValidator()
         {
-            RuleFor(p => p.Day).SetValidator(new DateDayValidator<UserInfo, int?>(m => m.Day));
-            RuleFor(p => p.Month).SetValidator(new DateMonthValidator<UserInfo, int?>(m => m.Month));
-            RuleFor(p => p.Year).SetValidator(new DateYearValidator<UserInfo, int?>(m => m.Year));
-            RuleFor(p => p.DoB).SetValidator(new DateOfBirthValidator<UserInfo, DateTime?>(m => m.DoB));
+            //RuleFor(p => p.Day).SetValidator(new DateDayValidator<UserInfo, int?>(m => m.Day));
+            //RuleFor(p => p.Month).SetValidator(new DateMonthValidator<UserInfo, int?>(m => m.Month));
+            //RuleFor(p => p.Year).SetValidator(new DateYearValidator<UserInfo, int?>(m => m.Year));
+            //RuleFor(p => p.DoB).SetValidator(new DateOfBirthValidator<UserInfo, DateTime?>(m => m.DoB));
         }
     }
 
