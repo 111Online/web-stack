baa3246476181fd1f8cc6bd09e1c1c68017fafba
james.tiffen@nhs.net <james.tiffen@nhs.net>
2020-05-11 14:15:57 +0100

    Added new DOB view models and associated validation

---
+++ NHS111/NHS111.Models/Models/Web/Validators/DateOfBirthViewModelValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using NHS111.Models.Models.Web.PersonalDetails;
+
+namespace NHS111.Models.Models.Web.Validators
+{
+    public class DateOfBirthViewModelValidator : AbstractValidator<DateOfBirthViewModel>
+    {
+        public DateOfBirthViewModelValidator()
+        {
+            RuleFor(p => p.Day).SetValidator(new DateDayValidator<UserInfo, int?>(m => m.Day));
+            RuleFor(p => p.Month).SetValidator(new DateMonthValidator<UserInfo, int?>(m => m.Month));
+            RuleFor(p => p.Year).SetValidator(new DateYearValidator<UserInfo, int?>(m => m.Year));
+            RuleFor(p => p.DoB).SetValidator(new DateOfBirthValidator<UserInfo, DateTime?>(m => m.DoB));
+        }
+    }
+}
