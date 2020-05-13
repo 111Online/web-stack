fa5e8f51ca692bbfb669eebb01d6cd0897f403d2
james.tiffen@nhs.net <james.tiffen@nhs.net>
2020-05-13 17:15:15 +0100

    Added awaiter to wait for element before continuing

--- NHS111/NHS111.Web.Functional.Utils/PageLoadAwaiter.cs
+++ NHS111/NHS111.Web.Functional.Utils/PageLoadAwaiter.cs
@@ -16,19 +16,25 @@ namespace NHS111.Web.Functional.Utils
 
         internal void AwaitNextPage(IWebElement currentHeader, bool expectQuestionPage = true)
         {
-            var timeout = TimeSpan.FromSeconds(20);
+            AwaitNextPage(currentHeader, By.CssSelector(".multiple-choice--radio"), 10, expectQuestionPage);
+        }
+
+        internal void AwaitNextPage(IWebElement currentHeader, By elementToWaitForBySelector, int timeoutSeconds =10, bool expectQuestionPage = true)
+        {
+            var timeout = TimeSpan.FromSeconds(timeoutSeconds);
             try
             {
                 if (expectQuestionPage)
                 {
-                    new WebDriverWait(_driver, timeout).Until(ExpectedConditions.ElementExists(By.CssSelector(".multiple-choice--radio")));
+                    new WebDriverWait(_driver, timeout).Until(ExpectedConditions.ElementExists(elementToWaitForBySelector));
                 }
             }
             catch (WebDriverTimeoutException)
             {
-                Assert.Fail(string.Format("The next question page didn't load in the awaited time ({0}s). Current page title is '{1}'.", timeout.Seconds, currentHeader.Text));
+                Assert.Fail(string.Format("The next page didn't load in the awaited time ({0}s). Current page title is '{1}'.", timeout.Seconds, currentHeader.Text));
             }
             new WebDriverWait(_driver, TimeSpan.FromSeconds(1));
         }
+
     }
 }
