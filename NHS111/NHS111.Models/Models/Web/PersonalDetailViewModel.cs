1804771fbe7a72c1df157b1e26101e9031901837
james.tiffen@nhs.net <james.tiffen@nhs.net>
2020-05-14 13:04:53 +0100

    Added new models and marked depreciated ones for removal once new views in place

--- NHS111/NHS111.Models/Models/Web/PersonalDetailViewModel.cs
+++ NHS111/NHS111.Models/Models/Web/PersonalDetailViewModel.cs
@@ -9,7 +9,7 @@ namespace NHS111.Models.Models.Web
     public class PersonalDetailViewModel : OutcomeViewModel
     {
         public LocationInfoViewModel AddressInformation { get; set; }
-        public PatientInformantViewModel PatientInformantDetails { get; set; }
+        public PatientInformantViewModel PatientInformantDetails { get; set; }     //To remove
         public EmailAddressViewModel EmailAddress { get; set; }
         public PersonalDetailViewModel() 
         {
