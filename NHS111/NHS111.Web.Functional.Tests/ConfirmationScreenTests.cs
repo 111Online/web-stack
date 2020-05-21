969310e6410f89a89818c82071d5ad173f39de8a
Nenyi Arkaah <nenyi.arkaah@gmail.com>
2020-05-21 16:53:55 +0100

    WIP: Fix to Midwifery for labour with resubmit scenario

--- NHS111/NHS111.Web.Functional.Tests/ConfirmationScreenTests.cs
+++ NHS111/NHS111.Web.Functional.Tests/ConfirmationScreenTests.cs
@@ -225,15 +225,19 @@ namespace NHS111.Web.Functional.Tests
             //need to select DoS ID 2000006999
             Driver.FindElement(By.XPath("//input[@value = '2000006999']"));
 
-            var personalDetailsPage = outcomePage.UseThisService("1");
-            personalDetailsPage.VerifyIsPersonalDetailsPage();
-            personalDetailsPage.SelectMe();
-            personalDetailsPage.EnterPatientName("Dx50 first", "Dx50 last");
-            personalDetailsPage.EnterDateOfBirth("31", "07", "1971");
-            personalDetailsPage.VerifyNameDisplayed();
-            personalDetailsPage.VerifyDateOfBirthDisplayed();
+            var personalDetailsInFormantPage = outcomePage.UseThisService("1");
+            personalDetailsInFormantPage.VerifyWhoNeedsHelpDisplayed();
+            personalDetailsInFormantPage.SelectMe();
 
-            var personalDetailsPhoneNumberPage = personalDetailsPage.SubmitPersonalDetails();
+            var personalDetailsNamePage = personalDetailsInFormantPage.SubmitPersonalDetails();
+            personalDetailsNamePage.EnterPatientName("Dx50 first", "Dx50 last");
+            personalDetailsNamePage.VerifyNameDisplayed();
+
+            var personalDetailsAgePage = personalDetailsNamePage.SubmitPersonalDetails();
+            personalDetailsAgePage.EnterDateOfBirth("31", "07", "1971");
+            personalDetailsAgePage.VerifyDateOfBirthDisplayed();
+
+            var personalDetailsPhoneNumberPage = personalDetailsAgePage.SubmitPersonalDetails();
             personalDetailsPhoneNumberPage.EnterPhoneNumberOnSeparatePage(telNumber);
             personalDetailsPhoneNumberPage.VerifyNumberDisplayedOnSeparatePage();
 
@@ -247,7 +251,7 @@ namespace NHS111.Web.Functional.Tests
             atHomePage.VerifyHeading("Are you at home?");
             atHomePage.SelectAtHomeYes();
 
-            var confirmDetails = personalDetailsPage.SubmitAtHome();
+            var confirmDetails = atHomePage.SubmitAtHome();
             confirmDetails.VerifyHeading("Check details");
             //need to submit call
             var callConfirmationPage = confirmDetails.SubmitCall();
