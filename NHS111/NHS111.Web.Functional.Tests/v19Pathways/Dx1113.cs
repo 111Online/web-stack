2a55f090ace7a7e713fb149c5d7a7dea6147213b
Nick Sparks <nick.sparks@nhs.net>
2020-05-30 11:26:28 +0100

    Setup structure for v19 tests and added first guided search test

---
+++ NHS111/NHS111.Web.Functional.Tests/v19Pathways/Dx1113.cs
@@ -0,0 +1,54 @@
+﻿using NHS111.Web.Functional.Utils;
+using NUnit.Framework;
+
+
+namespace NHS111.Web.Functional.Tests.v19Pathways
+
+{
+    [TestFixture]
+    class Dx1113 : BaseTests
+    {
+        private QuestionPage LaunchCovidWithLink(string sex, int age)
+        {
+            var homepage = TestScenarioPart.HomePage(Driver);
+            var covidHomePage = homepage.ClickCovidLink();
+            covidHomePage.VerifyCovidPathway();
+            var locationPage = covidHomePage.ClickOnStartNow();
+            var moduleZeroPage = TestScenarioPart.ModuleZero(locationPage);
+            var demographicsPage = TestScenarioPart.Demographics(moduleZeroPage);
+            return TestScenarioPart.Question(demographicsPage, sex, age);
+        }
+
+        [Test]
+        public void NavigateToDispositionDx1113()
+        {
+            var questionPage = LaunchCovidWithLink(TestScenerioSex.Female, TestScenerioAgeGroups.Adult);
+
+            var outcomePage = questionPage.AnswerText("SymptomsStart_Day", "6")
+                .Answer(2) // no
+                .Answer(1) // yes
+                .Answer(3) // no
+                .Answer(3) // no
+                .Answer(1) // Normal,warmorhot
+                .Answer<OutcomePage>(1); // Yes, I stopped doing everything what i do
+            outcomePage.VerifyHiddenField("Id", "Dx1113");
+        }
+
+        [Test]
+        public void NavigateToDispositionDx1113Journey2()
+        {
+            var questionPage = LaunchCovidWithLink(TestScenerioSex.Female, TestScenerioAgeGroups.Adult);
+
+            var outcomePage = questionPage.AnswerText("SymptomsStart_Day", "6")
+                .Answer(2) // no
+                .Answer(1) // yes
+                .Answer(3) // no
+                .Answer(3) // no
+                .Answer(1) // Normal,warmorhot
+                .Answer(3) // No - I feel well enough to do most of my usual daily activities 
+                .Answer<OutcomePage>(1); // Yes
+            outcomePage.VerifyHiddenField("Id", "Dx1113");
+            outcomePage.VerifyHasButton(buttonName: "PersonalDetails", buttonValue: "Book a call");
+        }
+    }
+}
---
+++ NHS111/NHS111.Web.Functional.Tests/v19Pathways/GuidedSearchTests.cs
@@ -0,0 +1,50 @@
+﻿using NHS111.Web.Functional.Utils;
+using NUnit.Framework;
+
+namespace NHS111.Web.Functional.Tests.v19Pathways
+{
+    public class GuidedSearchTests : BaseTests
+    {
+        private QuestionPage LaunchViaCovidLink(string sex, int age)
+        {
+            var homepage = TestScenarioPart.HomePage(Driver);
+            var covidHomePage = homepage.ClickCovidLink();
+            //covidHomePage.VerifyCovidPathway();
+            var locationPage = covidHomePage.ClickOnStartNow();
+            var moduleZeroPage = TestScenarioPart.ModuleZero(locationPage);
+            var demographicsPage = TestScenarioPart.Demographics(moduleZeroPage);
+            return TestScenarioPart.Question(demographicsPage, sex, age);
+        }
+
+        [Test]
+        public void NavigateToCoughPW975FemaleAdult()
+        {
+            var guidedSelectionPage = LaunchViaCovidLink(TestScenerioSex.Female, TestScenerioAgeGroups.Adult);
+
+            //Not question page, guidedselectionpage
+            var weirdQuestionPage = guidedSelectionPage.guidedSelection("Cough", true);
+
+            //Verify the covid message appears
+            //Confirm PathwayNo = PW854
+            weirdQuestionPage.VerifyHiddenField("PathwayNo", "PW975");
+
+            //click Iunderstand
+
+
+
+            //Now go through questions
+
+            //var outcomePage = questionPage.AnswerText("SymptomsStart_Day", "6")
+            //    .Answer(2) // no
+            //    .Answer(1) // yes
+            //    .Answer(3) // no
+            //    .Answer(3) // no
+            //    .Answer(1) // Normal,warmorhot
+            //    .Answer(3) // No - I feel well enough to do most of my usual daily activities 
+            //    .Answer(2) // I'm not sure
+            //    .Answer(2) // I'm not sure
+            //    .Answer<OutcomePage>(2); // I'm notsure
+            //outcomePage.VerifyHiddenField("Id", "Dx391");
+        }
+    }
+}
