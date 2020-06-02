beb9bffe39477f2a70c349f33877702e629f81ac
john hunter <john.hunter6@nhs.net>
2020-06-02 15:53:19 +0100

    adds the redirect to guided selection page

--- NHS111/NHS111.Web/Helpers/KeyValueEncryptor.cs
+++ NHS111/NHS111.Web/Helpers/KeyValueEncryptor.cs
@@ -1,4 +1,5 @@
 ﻿using NHS111.Models.Models.Web;
+using RestSharp.Extensions;
 
 namespace NHS111.Web.Helpers
 {
@@ -17,6 +18,20 @@ namespace NHS111.Web.Helpers
             encryptor["isCovidjourney"] = model.IsCovidJourney.ToString();
             encryptor["viaGuidedSelection"] = model.ViaGuidedSelection.HasValue ? model.ViaGuidedSelection.Value.ToString() : string.Empty;
 
+            return encryptor.ToString();
+        }
+        public static string EncryptedKeys(JustToBeSafeViewModel model)
+        {
+            var encryptor = new QueryStringEncryptor();
+            encryptor["sessionId"] = model.SessionId.ToString();
+            encryptor["postcode"] = !string.IsNullOrEmpty(model.CurrentPostcode) ? model.CurrentPostcode : string.Empty;
+            encryptor["filterServices"] = model.FilterServices.ToString();
+            encryptor["campaign"] = !string.IsNullOrEmpty(model.Campaign) ? model.Campaign : string.Empty;
+            encryptor["source"] = !string.IsNullOrEmpty(model.Source) ? model.Source : string.Empty;
+            encryptor["digitalTitle"] = !string.IsNullOrEmpty(model.DigitalTitle) ? model.DigitalTitle : string.Empty;
+            encryptor["isCovidjourney"] = model.IsCovidJourney.ToString();
+            encryptor["viaGuidedSelection"] = model.ViaGuidedSelection.HasValue ? model.ViaGuidedSelection.Value.ToString() : string.Empty;
+            encryptor["searchTerm"] = !string.IsNullOrWhiteSpace(model.EntrySearchTerm) ? model.EntrySearchTerm : string.Empty;
             return encryptor.ToString();
         }
     }
