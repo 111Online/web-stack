b7ce5c78c9eb4ba5ab834a23cd61d2edb0c5e1bf
Harry Hawkins <harry.hawkins1@nhs.net>
2020-08-05 12:02:30 +0000

    This removes the style element from the emergency alert.
    It was there to hide the border on the global header when the yellow box
    is shown, however it was implemented quickly using a <style> element
    which is only valid HTML within the <head>. So this replaces it with a
    class that is injected into the header when the section for emergency
    banner exists.

--- NHS111/NHS111.Models/Models/Web/Elements/LinkSummaryViewModel.cs
+++ NHS111/NHS111.Models/Models/Web/Elements/LinkSummaryViewModel.cs
@@ -5,5 +5,7 @@
         public string Link { get; set; }
         public string Summary { get; set; }
         public string ID { get; set; }
+        public string EventTrigger { get; set; }
+        public string EventValue { get; set; }
     }
 }
