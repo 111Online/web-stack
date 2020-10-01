b22bc2b15becf484329887171ee55f93b2c90c0c
john hunter <john.hunter6@nhs.net>
2020-10-01 16:18:08 +0100

    shows correct confirmation screen for itk - fixes bug where disposition time is available but datetime now is used

--- NHS111/NHS111.Models/Models/Web/ReferralResultViewModels/ConfirmationReferral/AccidentAndEmergencySexualAssaultReferralConfirmationResultViewModel.cs
+++ NHS111/NHS111.Models/Models/Web/ReferralResultViewModels/ConfirmationReferral/AccidentAndEmergencySexualAssaultReferralConfirmationResultViewModel.cs
@@ -3,7 +3,7 @@
     public class AccidentAndEmergencySexualAssaultReferralConfirmationResultViewModel
         : ReferralConfirmationResultViewModel
     {
-        public override string ViewName { get { return string.Format("Confirmation/SexualAssault/Confirmation"); } }
+        public override string ViewName { get { return string.Format("Confirmation/defaultWithDetails/Confirmation"); } }
         public override string PageTitle{ get { return "Your call is confirmed"; } }
 
         public AccidentAndEmergencySexualAssaultReferralConfirmationResultViewModel(ITKConfirmationViewModel itkConfirmationViewModel)
